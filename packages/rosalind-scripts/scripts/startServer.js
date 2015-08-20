4007d75a60ce85eeda1f268403e58208267e378e
Albert Zak <me@albertzak.com>
2015-08-20 13:33:59 +0200

    Add sanjo:jasmine

--- packages/rosalind-scripts/scripts/startServer.js
+++ packages/rosalind-scripts/scripts/startServer.js
@@ -6,5 +6,9 @@ cd('../../..');
 require('dns').lookup(require('os').hostname(), function (err, ip, fam) {
   var rootUrl = 'http://' + ip + ':3000';
   console.log('Listening on ' + rootUrl);
-  exec('ROOT_URL=' + rootUrl + ' meteor --settings settings.json');
+  exec([
+    'ROOT_URL=' + rootUrl,
+    'JASMINE_BROWSER=PhantomJS',
+    'meteor --settings settings.json'
+  ].join(' '));
 })
