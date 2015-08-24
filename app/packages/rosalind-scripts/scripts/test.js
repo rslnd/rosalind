100f8b39112d6b7f330000f03f420878050ed983
Albert Zak <me@albertzak.com>
2015-08-24 12:44:50 +0200

    Add warning when running npm test on ci

--- app/packages/rosalind-scripts/scripts/test.js
+++ app/packages/rosalind-scripts/scripts/test.js
@@ -1,11 +1,19 @@
 require('shelljs/global');
 
-cd(__dirname);
-cd('../../..');
-exec([
+var testCommand = [
   'VELOCITY_CI=1',
   'meteor',
   '--test',
   '--settings ../environments/test/settings.json',
   '--release velocity:METEOR@1.1.0.3_1'
-].join(' '));
+].join(' ');
+
+if (process.env.CI) {
+  console.warn('** Note: Do not run tests with this script on CI');
+  console.warn('** Note: Shelljs always returns zero exit code even when tests fail');
+  console.log('** Use instead:\n' + testCommand);
+}
+
+cd(__dirname);
+cd('../../..');
+exec(testCommand);
