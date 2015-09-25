c3318bf0e2ea53e89c70f56e99c1ace17c731b6b
Albert Zak <me@albertzak.com>
2015-09-25 15:19:02 +0200

    Run jasmine server integration tests on ci

--- app/packages/rosalind-scripts/scripts/ci/test.sh
+++ app/packages/rosalind-scripts/scripts/ci/test.sh
@@ -1,1 +1,1 @@
-VELOCITY_CI=1 JASMINE_CLIENT_UNIT=1 JASMINE_BROWSER=Chrome SELENIUM_BROWSER=Chrome meteor --test --settings ../../../../../environments/test/settings.json
+VELOCITY_CI=1 JASMINE_CLIENT_UNIT=1 JASMINE_SERVER_INTEGRATION=1 JASMINE_BROWSER=Chrome SELENIUM_BROWSER=Chrome meteor --test --settings ../../../../../environments/test/settings.json
--- app/packages/rosalind-scripts/scripts/test.js
+++ app/packages/rosalind-scripts/scripts/test.js
@@ -3,7 +3,7 @@ require('shelljs/global');
 var testCommand = [
   'VELOCITY_CI=1',
   'JASMINE_CLIENT_UNIT=1',
-  'JASMINE_SERVER_INTEGRATION=0',
+  'JASMINE_SERVER_INTEGRATION=1',
   'JASMINE_BROWSER=Chrome',
   'SELENIUM_BROWSER=Chrome',
   'meteor',
