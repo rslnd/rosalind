38cb89e259008967f142f050e1c12af0cd8dcb56
Albert Zak <me@albertzak.com>
2015-09-30 19:23:35 +0200

    Add test for comments

--- app/meteor/tests/cucumber/features/step_definitions/assertionSteps.js
+++ app/meteor/tests/cucumber/features/step_definitions/assertionSteps.js
@@ -6,14 +6,22 @@
 
     this.Then('I should see \'$string\'', function (string) {
       client.waitForExist('#loaded');
-      var mainText = client.getText('#main');
+      var mainText = client.getText('body');
       expect(mainText.toLowerCase()).toContain(string.toLowerCase());
     });
 
     this.Then('I should not see \'$string\'', function (string) {
       client.waitForExist('#loaded');
-      var mainText = client.getText('#main');
+      var mainText = client.getText('body');
       expect(mainText.toLowerCase()).not.toContain(string.toLowerCase());
+    });
+
+    this.Then('I should see \'$string\' in \'$element\'', function (string, element) {
+      client.waitForExist(element);
+      var elementText = client.getText(element);
+      if (typeof elementText === 'object')
+        elementText = elementText.join(' ');
+      expect(elementText.toLowerCase()).toContain(string.toLowerCase());
     });
 
     this.Then('the field \'$labelText\' should be empty', function (labelText) {
@@ -28,9 +36,12 @@
       var time = moment().format('HH:mm');
 
       client.waitForExist('#loaded');
-      var bodyText = client.getText('#main');
+      var bodyText = client.getText('body');
       expect(bodyText).toContain(time.slice(0, -1));
     });
 
+    this.Then('I should see the element \'$selector\'', function (selector) {
+      client.waitForVisible(selector);
+    });
   };
 })();
