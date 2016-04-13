b1b1c45285fe52fc2791dd2ef500d70610d42aaf
Albert Zak <me@albertzak.com>
2016-04-13 21:45:09 +0200

    💨 Push to resin

--- environments/production/build.sh
+++ environments/production/build.sh
@@ -12,6 +12,8 @@ docker-compose -f ../development/docker-compose.yml run meteor bash -c 'rm -rf /
   && meteor build --architecture=os.linux.x86_64 --server=http://0.0.0.0 --directory /build/ \
   && cp /app/package.json /build/bundle/'
 
+./resin.sh
+
 cd -
 
 echo "** Done building meteor bundle"
