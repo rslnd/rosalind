0b1ceb2f2205a21deeaa048af7c068e7154e34e8
Albert Zak <me@albertzak.com>
2016-04-13 20:07:16 +0200

    🏡 Add meteor build step to ci matrix

---
+++ environments/production/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "** Building meteor bundle"
+
+cd "$(dirname "$0")"
+
+docker-compose -f ../development/docker-compose.yml run meteor bash -c 'rm -rf /build/* \
+  && meteor build --architecture=os.linux.x86_64 --server=http://0.0.0.0 --directory /build/ \
+  && cp /app/package.json /build/bundle/'
+
+cd -
+
+echo "** Done building meteor bundle"
