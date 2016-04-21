946440448e434942d619007dda89a34e7486e266
Albert Zak <me@albertzak.com>
2016-04-21 14:56:15 +0200

    😇 Update ci script

--- ci.sh
+++ ci.sh
@@ -6,10 +6,16 @@ YML="-f docker-compose.yml -f docker-compose.test.yml"
 
 case "$1" in
   test)
+    if [ -z "$ROOT_URL" ]; then
+      echo "Please set ROOT_URL for running integration tests"
+      exit 1
+    fi
+
     docker-compose $YML pull
+    cd app/meteor/tests/cucumber && npm install && cd -
     docker-compose $YML run meteor meteor npm install
     npm run start:test &
-    for i in {1..180}; do curl 'http://0.0.0.0:3000' && break; sleep 1; done;
+    for i in {1..180}; do curl "$ROOT_URL" && break; sleep 1; done;
     npm test
 
     ;;
