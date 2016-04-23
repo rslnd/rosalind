48fccefc2d4238bc5af6b87aed9557ca7b2051c5
Albert Zak <me@albertzak.com>
2016-04-23 22:36:28 +0200

    🐓 Clean up build output on ci

--- production/build.sh
+++ production/build.sh
@@ -6,7 +6,10 @@ echo "** Building app"
 
 cd "$(dirname "$0")"
 
+echo -en "travis_fold:start:pack\r"
 chmod +x ./pack.sh
+echo -en "travis_fold:end:pack\r"
+
 chmod +x ./x86/build.sh
 chmod +x ./arm/build.sh
 
