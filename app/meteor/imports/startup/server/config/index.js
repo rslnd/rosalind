2bcf1c225df52d987ee264f812b44b8229fad2ce
Albert Zak <me@albertzak.com>
2016-05-20 21:08:19 +0200

    😋 Fix import

--- app/meteor/imports/startup/server/config/index.js
+++ app/meteor/imports/startup/server/config/index.js
@@ -2,6 +2,7 @@ import accounts from './accounts'
 import appcache from './appcache'
 import defaultAccount from './defaultAccount'
 import browserPolicy from './browserPolicy'
+import test from './test'
 
 export default function() {
   test()
