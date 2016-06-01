0cd4a10ce136b8d2d033024a1903e7ba2f13a076
Albert Zak <me@albertzak.com>
2016-06-01 13:02:22 +0200

    👬 Fix paths

--- production/prepare.sh
+++ production/prepare.sh
@@ -9,10 +9,6 @@
 
 set -e
 
-cd "$(dirname "$0")"
-
-echo "** "
-
 DOCKER_TAG="$(git rev-parse --verify HEAD)"
 DOCKER_TAG="${DOCKER_TAG:0:12}"
 
@@ -22,5 +18,3 @@ chmod +x build.sh
 chmod +x image.sh
 chmod +x push.sh
 chmod +x deploy.sh
-
-cd -
