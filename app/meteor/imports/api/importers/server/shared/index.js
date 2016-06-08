3cb8619a3c32261d06cc0f008f2d51219418c3b2
Albert Zak <me@albertzak.com>
2016-06-08 21:49:49 +0200

    🦄 Refactor importers, add ddp upload handler

--- app/meteor/imports/api/importers/server/shared/index.js
+++ app/meteor/imports/api/importers/server/shared/index.js
@@ -2,12 +2,10 @@ import adt from './adt'
 import bulk from './bulk'
 import csv from './csv'
 import mdb from './mdb'
-import uploadStream from './uploadStream'
 
 export {
   adt,
   bulk,
   csv,
-  mdb,
-  uploadStream
+  mdb
 }
