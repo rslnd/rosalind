7658f604a35e2591c36cd210887909f6292cf4f3
Albert Zak <me@albertzak.com>
2016-06-30 16:25:45 +0200

    👰 Prefer functional stateless components

--- app/meteor/client/ui/users/components/Avatar.jsx
+++ app/meteor/client/ui/users/components/Avatar.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { Users } from 'api/users'
 
-export class Avatar extends React.Component {
-  render () {
-    let user
+export const Avatar = ({ userId }) => {
+  let user
 
-    if (this.props.userId) {
-      user = Users.findOne({ _id: this.props.userId })
-    } else {
-      user = Meteor.user()
-    }
-
-    return (<div className='avatar username img-sm'>{user.shortname()}</div>)
+  if (userId) {
+    user = Users.findOne({ _id: userId })
+  } else {
+    user = Meteor.user()
   }
+
+  return (<div className="avatar username img-sm">{user.shortname()}</div>)
 }
