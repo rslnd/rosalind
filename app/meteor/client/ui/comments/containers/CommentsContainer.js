527cb319f76f405e32e4eba0e1053a86e40881b0
Albert Zak <me@albertzak.com>
2016-06-30 16:01:49 +0200

    ⚒ Use jsx on client side

---
+++ app/meteor/client/ui/comments/containers/CommentsContainer.js
@@ -0,0 +1,12 @@
+import { Comments } from 'api/comments'
+import { CommentsBox } from '../components/Commentsbox'
+import { composeWithTracker } from 'react-komposer'
+
+const composer = (props, onData) => {
+  const comments = Comments.find({ docId: props.docId }, { sort: { createdAt: 1 } }).fetch()
+  onData(null, { comments, docId: props.docId })
+}
+
+const CommentsContainer = composeWithTracker(composer)(CommentsBox)
+
+export { CommentsContainer }
