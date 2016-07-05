ff5ad05eb5b10c2bc83642358d06a2439d6ed63f
Albert Zak <me@albertzak.com>
2016-07-05 14:22:25 +0200

    😍 Add enter/leave animations to inbound calls! Wohoo!

--- app/meteor/client/ui/components/Box.jsx
+++ app/meteor/client/ui/components/Box.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export const Box = ({ title, type, body }) => (
-  <div className={`box box-${type || 'default'}`}>
-    <div className="box-header with-border">
-      <h3 className="box-title">{title}</h3>
-    </div>
-    <div className="box-body">{body}</div>
-  </div>
-)
+export class Box extends React.Component {
+  render () {
+    return (
+      <div className={`box box-${this.props.type || 'default'}`}>
+        <div className="box-header with-border">
+          <h3 className="box-title">{this.props.title}</h3>
+        </div>
+        <div className="box-body">{this.props.body}</div>
+      </div>
+    )
+  }
+}
