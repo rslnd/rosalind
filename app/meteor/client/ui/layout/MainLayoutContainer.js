fe9c4a584d5b99ee18d25b1c1558ec6498850d7c
Albert Zak <me@albertzak.com>
2016-07-06 15:00:34 +0200

    🙈 Add markup for login screen

--- app/meteor/client/ui/layout/MainLayoutContainer.js
+++ app/meteor/client/ui/layout/MainLayoutContainer.js
@@ -1,21 +1,25 @@
-import React from 'react'
 import { composeWithTracker } from 'react-komposer'
 import { Meteor } from 'meteor/meteor'
 import { MainLayout } from './MainLayout'
 
 const composer = (props, onData) => {
-  let handles = []
+  const currentUser = Meteor.user()
 
-  handles.push(Meteor.subscribe('users'))
-  handles.push(Meteor.subscribe('cache'))
-  handles.push(Meteor.subscribe('groups'))
-  handles.push(Meteor.subscribe('tags'))
-  handles.push(Meteor.subscribe('schedules'))
-  handles.push(Meteor.subscribe('timesheets'))
-  handles.push(Meteor.subscribe('inboundCalls'))
+  if (currentUser) {
+    let handles = []
 
-  if (handles.every((h) => h.ready())) {
-    const currentUser = Meteor.user()
+    handles.push(Meteor.subscribe('users'))
+    handles.push(Meteor.subscribe('cache'))
+    handles.push(Meteor.subscribe('groups'))
+    handles.push(Meteor.subscribe('tags'))
+    handles.push(Meteor.subscribe('schedules'))
+    handles.push(Meteor.subscribe('timesheets'))
+    handles.push(Meteor.subscribe('inboundCalls'))
+
+    if (handles.every((h) => h.ready())) {
+      onData(null, { ...props, currentUser })
+    }
+  } else {
     onData(null, { ...props, currentUser })
   }
 }
--- app/meteor/client/ui/users/Login.jsx
+++ app/meteor/client/ui/users/Login.jsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import { TAPi18n } from 'meteor/tap:i18n'
 
 export const Login = () => (
-  <div></div>
+  <form className="login form-horizontal">
+    <div className="panel panel-default">
+      <div className="panel-heading">
+        <h3 className="panel-title">{TAPi18n.__('login.heading')}</h3>
+      </div>
+      <div className="panel-body">
+        <div className="form-group">
+          <label className="sr-only" for="nameField">{TAPi18n.__('login.name')}</label>
+          <div className="col-sm-12">
+            <input name="name" id="nameField" className="input-lg form-control" placeholder={TAPi18n.__('login.form.name.placeholder')} />
+          </div>
+        </div>
+        <div className="form-group">
+          <label className="sr-only" for="passwordField">{TAPi18n.__('login.password')}</label>
+          <div className="col-sm-12">
+            <input type="password" name="password" id="passwordField" className="input-lg form-control" placeholder={TAPi18n.__('login.form.password.placeholder')} />
+          </div>
+        </div>
+        <div className="form-group no-mb">
+          <div className="col-sm-12">
+            <button className="btn btn-success btn-block btn-lg" type="submit">{TAPi18n.__('login.button')}</button>
+          </div>
+        </div>
+      </div>
+      <div className="panel-footer">
+        <a className="text-muted">{TAPi18n.__('login.help.show')}</a>
+      </div>
+    </div>
+  </form>
 )
