544cd0057c1c9317bbe9380f0785e3ece45fe96c
Albert Zak <me@albertzak.com>
2016-07-10 08:52:55 +0200

    ⛩ Add report component

---
+++ app/meteor/client/ui/reports/Report.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import FlipMove from 'react-flip-move'
+import { UserHelper } from 'client/ui/users/UserHelper'
+import { TAPi18n } from 'meteor/tap:i18n'
+
+export const ReportTableHeader = ({ showRevenue }) => (
+  <thead>
+    <tr>
+      <th>#</th>
+      <th className="col-md-2">Arzt</th>
+      <th>Stunden</th>
+      <th className="td-bg" width="150px">Patienten</th>
+      <th className="td-bg">Neu</th>
+      <th className="td-bg">Kontrolle</th>
+      <th className="td-bg">Neu/h</th>
+      <th className="td-bg">Gesamt</th>
+      <th className="OP">OP</th>
+      {showRevenue && <th>Umsatz</th>}
+    </tr>
+  </thead>
+)
+
+export const ReportTableBody = ({ showRevenue, report }) => (
+  <FlipMove
+    duration={260}
+    typeName="tbody"
+    leaveAnimation="fade"
+    staggerDelayBy={70}
+    staggerDurationBy={70}>
+    {report.assignees.map((assignee, index) => (
+      <tr key={assignee.id}>
+        <td>{index + 1}</td>
+        <td>
+          {
+            assignee.id
+            ? <UserHelper userId={assignee.id} />
+            : <i className="text-muted">{TAPi18n.__('reports.unassigned')}</i>
+          }
+        </td>
+        <td>
+          {assignee.hours.actual && assignee.hours.actual.toFixed(1)}
+          {assignee.hours.scheduled && (
+            <span>
+              {assignee.hours.scheduled.toFixed(1)}
+              <small className="fa fa-question-circle text-quite-muted">{TAPi18n.__('reports.scheduledOnly')}</small>
+            </span>
+          )}
+        </td>
+        <td className="td-bg on-hover-here">
+          <div className="progress bg-aqua-light">
+            <div className="progress-bar progress-bar-aqua" style={{width: `${assignee.patients.newPercentage}%`}}>
+              <div className="on-hover-show show-print">{Math.floor(assignee.patients.newPercentage)}% {TAPi18n.__('reports.newPercentage')}</div>
+            </div>
+          </div>
+        </td>
+        <td className="td-bg">{assignee.patients.new}</td>
+        <td className="td-bg">{assignee.patients.recall}</td>
+        <td className="td-bg">{assignee.patients.newPerHourScheduled.toFixed(1)}</td>
+        <td className="td-bg">{assignee.patients.total}</td>
+        <td>{assignee.patients.surgeries}</td>
+        {showRevenue && <td>€{assignee.revenue}</td>}
+      </tr>
+    ))}
+  </FlipMove>
+)
+
+export const Report = ({ report, showRevenue }) => (
+  <div className="box box-info">
+    <div className="box-body no-padding">
+      <div className="table-responsive">
+        <table className="table no-margin">
+          <ReportTableHeader showRevenue={showRevenue} />
+          <ReportTableBody report={report} showRevenue={showRevenue} />
+        </table>
+      </div>
+    </div>
+  </div>
+)
--- app/meteor/client/ui/reports/ReportsScreen.jsx
+++ app/meteor/client/ui/reports/ReportsScreen.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import FlipMove from 'react-flip-move'
+import { TAPi18n } from 'meteor/tap:i18n'
+import { weekOfYear } from 'util/time/format'
 import { DateNavigation } from 'client/ui/components/DateNavigation'
+import { Box } from 'client/ui/components/Box'
+import { Report } from './Report'
 
 export const ReportsScreen = ({ date, report }) => (
-  <div className="content">
-    <DateNavigation date={date} basePath="reports" />
-    <pre>{JSON.stringify(report)}</pre>
+  <div>
+    <div className="content-header">
+      <h1>
+        {TAPi18n.__('reports.thisDaySingular')} {date.calendar()}
+        <small>{weekOfYear(date)}</small>
+      </h1>
+      <DateNavigation date={date} basePath="reports" pullRight />
+    </div>
+    <div className="content">
+      <FlipMove duration={230}>
+        {
+          report
+          ? <div key="reportTable"><Report report={report} /></div>
+        : <div key="noReports"><Box type="warning" title={TAPi18n.__('ui.notice')} body={TAPi18n.__('reports.empty')} /></div>
+        }
+      </FlipMove>
+    </div>
   </div>
 )
