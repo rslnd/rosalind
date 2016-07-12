36ee7b41df77fa0aca95f8cabac04454753aa014
Albert Zak <me@albertzak.com>
2016-07-12 09:17:29 +0200

    🐗 Add fontawesome to react storybook

--- app/meteor/.storybook/webpack.config.js
+++ app/meteor/.storybook/webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
         test: /\.css?$/,
         loaders: [ 'style', 'raw' ],
         include: path.resolve(__dirname, '../')
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
       }
     ]
   },
