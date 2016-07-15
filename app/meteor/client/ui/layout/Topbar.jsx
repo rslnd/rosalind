1fc6864897bdd0803e2376cee60861d49b96198c
Albert Zak <me@albertzak.com>
2016-07-15 13:44:53 +0200

    🌘 Add logout button

--- app/meteor/client/ui/layout/Topbar.jsx
+++ app/meteor/client/ui/layout/Topbar.jsx
@@ -1,33 +1,71 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Button } from 'react-bootstrap'
+import { TAPi18n } from 'meteor/tap:i18n'
+import { Icon } from 'client/ui/components/Icon'
 
-export const Topbar = ({ toggleSidebar }) => (
-  <header className="main-header">
-    <Link to="/" className="logo">
-      <img src="/images/logo.svg" />
-    </Link>
+export class Topbar extends React.Component {
+  constructor (props) {
+    super(props)
 
-    <nav className="navbar navbar-static-top">
-      <ul className="nav navbar-nav navbar-left">
-        <li>
-          <a onClick={() => toggleSidebar()}>
-            <i className="fa fa-bars"></i>
-          </a>
-        </li>
-      </ul>
+    this.state = {
+      userMenuOpen: false
+    }
 
-      <div className="navbar-custom-menu">
-        <ul className="nav navbar-nav">
-          <li className="dropdown user user-menu">
-            <a>
-              <span>
-                Username
-                <i className="caret"></i>
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-)
+    this.toggleUserMenu = this.toggleUserMenu.bind(this)
+  }
+
+  toggleUserMenu () {
+    this.setState({
+      ...this.state,
+      userMenuOpen: !this.state.userMenuOpen
+    })
+  }
+
+  render () {
+    return (
+      <header className="main-header">
+        <Link to="/" className="logo">
+          <img src="/images/logo.svg" />
+        </Link>
+
+        <nav className="navbar navbar-static-top">
+          <ul className="nav navbar-nav navbar-left">
+            <li>
+              <a onClick={this.props.toggleSidebar}>
+                <i className="fa fa-bars"></i>
+              </a>
+            </li>
+          </ul>
+
+          <div className="navbar-custom-menu">
+            <ul className="nav navbar-nav">
+              <li className={`dropdown user user-menu ${this.state.userMenuOpen && 'open'}`}>
+                <a onClick={this.toggleUserMenu}>
+                  <span >
+                    {this.props.username}
+                    <i className="caret" />
+                  </span>
+                </a>
+                <ul className="dropdown-menu">
+                  <li className="user-body">{this.props.username}</li>
+                  <li className="user-footer">
+                    {
+                      this.props.loggingOut
+                      ? <Button bsStyle="default" block disabled>
+                        <Icon name="refresh" spin />
+                      </Button>
+                      : <Button bsStyle="default" block onClick={this.props.handleLogout}>
+                        {TAPi18n.__('login.logout')} <Icon name="sign-out" />
+                      </Button>
+                    }
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    )
+  }
+}
--- app/meteor/client/ui/layout/TopbarContainer.js
+++ app/meteor/client/ui/layout/TopbarContainer.js
@@ -1,10 +1,45 @@
+import React from 'react'
 import { composeWithTracker } from 'react-komposer'
+import { Meteor } from 'meteor/meteor'
 import { Topbar } from './topbar'
 
-const composer = (props, onData) => {
-  onData(null, {})
+class TopbarContainerComponent extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      loggingOut: false
+    }
+
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  handleLogout () {
+    this.setState({
+      ...this.state,
+      loggingOut: true
+    })
+
+    Meteor.call('users/logout', () => {
+      Meteor.logout()
+    })
+  }
+
+  render () {
+    return (
+      <Topbar
+        username={this.props.username}
+        handleLogout={this.handleLogout}
+        loggingOut={this.state.loggingOut} />
+    )
+  }
 }
 
-const TopbarContainer = composeWithTracker(composer)(Topbar)
+const composer = (props, onData) => {
+  const username = Meteor.user().fullNameWithTitle()
+  onData(null, { username })
+}
+
+const TopbarContainer = composeWithTracker(composer)(TopbarContainerComponent)
 
 export { TopbarContainer }
