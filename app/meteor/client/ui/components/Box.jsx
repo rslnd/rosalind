e3d064c7bd386c3fc53598335037713d1dc85b54
Albert Zak <me@albertzak.com>
2016-08-02 11:06:54 +0200

    ⛑ Collapse box if no title passed

--- app/meteor/client/ui/components/Box.jsx
+++ app/meteor/client/ui/components/Box.jsx
@@ -5,11 +5,13 @@ export class Box extends React.Component {
   render () {
     return (
       <div className={`box box-${this.props.type || 'default'}`}>
-        <div className="box-header with-border">
-          {this.props.icon && <Icon name={this.props.icon} />}
-          <h3 className="box-title">{this.props.title}</h3>
-        </div>
-        <div className="box-body">{this.props.body || this.props.children}</div>
+        {this.props.title &&
+          <div className="box-header with-border">
+            {this.props.icon && <Icon name={this.props.icon} />}
+            <h3 className="box-title">{this.props.title}</h3>
+          </div>
+        }
+        <div className={`box-body ${this.props.noPadding && 'no-padding'}`}>{this.props.body || this.props.children}</div>
       </div>
     )
   }
