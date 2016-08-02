f50394d3ff08aa0d5ae100da41e73064057ccbb3
Albert Zak <me@albertzak.com>
2016-08-02 10:34:25 +0200

    🐒 Expose api global on client in development

--- app/meteor/startup/client/config/index.js
+++ app/meteor/startup/client/config/index.js
@@ -1,9 +1,11 @@
+import development from './development'
 import alerts from './alerts'
 import hotkeys from './hotkeys'
 import locale from './locale'
 import userStatus from './userStatus'
 
 export default function () {
+  development()
   alerts()
   locale()
   userStatus()
--- app/meteor/startup/server/config/env.js
+++ app/meteor/startup/server/config/env.js
@@ -2,6 +2,7 @@ import { Env } from 'meteor/clinical:env'
 
 export default () => {
   Env.allow({
+    NODE_ENV: true,
     COMMIT_HASH: true,
     BUILD_NUMBER: true,
     CUSTOMER_NAME: true
