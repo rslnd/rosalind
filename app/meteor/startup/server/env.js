b78c3f30c430b07a5aea9515989d7e8ed25cc29f
Albert Zak <me@albertzak.com>
2016-09-09 19:56:17 +0200

    📿 Allow reading env var `test` on client

--- app/meteor/startup/server/env.js
+++ app/meteor/startup/server/env.js
@@ -3,6 +3,7 @@ import { Env } from 'meteor/clinical:env'
 export default () => {
   Env.allow({
     NODE_ENV: true,
+    TEST: true,
     COMMIT_HASH: true,
     BUILD_NUMBER: true,
     CUSTOMER_NAME: true,
