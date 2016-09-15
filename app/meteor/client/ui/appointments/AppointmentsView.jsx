50c2b061ad1eb82b19e32cc92f36b4ec7ab7b6cd
Albert Zak <me@albertzak.com>
2016-09-15 13:00:17 +0200

    🍕 YAY CSS GRIDS

--- app/meteor/client/ui/appointments/AppointmentsView.jsx
+++ app/meteor/client/ui/appointments/AppointmentsView.jsx
@@ -1,19 +1,48 @@
+import moment from 'moment'
+import range from 'lodash/range'
 import React from 'react'
-import { TAPi18n } from 'meteor/tap:i18n'
 import style from './style'
 
 export class AppointmentsView extends React.Component {
+  // row name    | column names
+  // ---------------------------------------------------------------
+  // [header]    | [time] [assignee-1] [assignee-2] ... [assignee-n]
+  // [time-800]  | [time] [assignee-1] [assignee-2] ... [assignee-n]
+  // [time-805]  | [time] [assignee-1] [assignee-2] ... [assignee-n]
+  // [time-810]  | [time] [assignee-1] [assignee-2] ... [assignee-n]
+  // ...         | ...
+  // [time-2100] | [time] [assignee-1] [assignee-2] ... [assignee-n]
+  grid () {
+    const timeRange = range(700, 2200, 5)
+    return {
+      display: 'grid',
+      gridTemplateColumns: `[time] 60px ${this.props.assignees.map((assignee) =>
+        `[assignee-${assignee.assigneeId}] auto`).join(' ')}`,
+      gridTemplateRows: `[header] 40px [subheader] 40px ${timeRange.map((time) => `[time-${time}] 18px`).join(' ')}`
+    }
+  }
+
   render () {
     return (
-      <div className={style.grid}>
-        <div></div>
-        <div className={style.header}>
-          {this.props.assignees.map((assignee) => (
-            <div key={assignee.assigneeId} className={style.headerCell}>
-              {assignee.fullNameWithTitle}
+      <div style={this.grid()}>
+        {this.props.assignees.map((assignee) => (
+          <div key={assignee.assigneeId} className={style.headerCell} style={{
+            gridRow: 'header',
+            gridColumn: `assignee-${assignee.assigneeId}`
+          }}>
+            {assignee.fullNameWithTitle}
+          </div>
+        ))}
+        {this.props.assignees.map((assignee) => (
+          assignee.appointments.map((appointment) => (
+            <div key={appointment._id} className={style.appointment} style={{
+              gridRow: moment(appointment.start).format('[time-]Hmm'),
+              gridColumn: `assignee-${assignee.assigneeId}`
+            }}>
+              {appointment.start.toString().substr(15, 9)}
             </div>
-          ))}
-        </div>
+          ))
+        ))}
       </div>
     )
   }
