c27afc66ffb55d35866a3777730631c43c078894
Albert Zak <me@albertzak.com>
2016-09-30 15:04:27 +0200

    🙆 Add permissive websocket browser policy

--- app/meteor/startup/server/browserPolicy.js
+++ app/meteor/startup/server/browserPolicy.js
@@ -4,6 +4,7 @@ export default () => {
   BrowserPolicy.framing.disallow()
   BrowserPolicy.content.allowInlineScripts()
   BrowserPolicy.content.allowEval()
+  BrowserPolicy.content.allowConnectOrigin('*')
 
   BrowserPolicy.content.allowInlineStyles()
   BrowserPolicy.content.allowStyleOrigin('https://fonts.googleapis.com')
@@ -12,7 +13,7 @@ export default () => {
 
   BrowserPolicy.content.allowScriptOrigin('https://cdn.smooch.io/')
   BrowserPolicy.content.allowScriptOrigin('https://api.smooch.io')
-  BrowserPolicy.content.allowFontOrigin('https://*.bootstrapcdn.com/')
+  BrowserPolicy.content.allowFontOrigin('https://*.bootstrapcdn.com')
   BrowserPolicy.content.allowImageOrigin('https://cdn.smooch.io/')
   BrowserPolicy.content.allowImageOrigin('https://api.smooch.io')
   BrowserPolicy.content.allowImageOrigin('https://app.smooch.io')
@@ -22,6 +23,6 @@ export default () => {
   if (process.env.NODE_ENV !== 'production') {
     BrowserPolicy.content.allowScriptOrigin('http://localhost:3500')
     BrowserPolicy.content.allowFontOrigin('http://localhost:3500')
-    BrowserPolicy.content.allowFontOrigin('http://*.bootstrapcdn.com/')
+    BrowserPolicy.content.allowFontOrigin('http://*.bootstrapcdn.com')
   }
 }
