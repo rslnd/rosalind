c2f4e51660510beeb7ac94c525806683c5dfc284
Albert Zak <me@albertzak.com>
2016-11-01 22:06:05 +0100

    🌂 Ignore errors thrown by smooch

--- app/meteor/startup/client/kadira.js
+++ app/meteor/startup/client/kadira.js
@@ -4,6 +4,15 @@ export default () => {
   Kadira.errors.addFilter(Kadira.errorFilters.filterCommonMeteorErrors)
   Kadira.errors.addFilter((errorType, message, error) => {
     console.warn('[Client] Uncaught Error', {errorType, message, error})
+
+    // Ignore errors thrown by Smooch
+    if (!error && (
+        message === "Cannot read property 'status' of undefined" ||
+        message === "Cannot read property 'appUser' of undefined" ||
+        message === 'Not found')) {
+      return false
+    }
+
     return true
   })
 }
