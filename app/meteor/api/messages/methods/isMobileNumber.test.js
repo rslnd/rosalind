e39d2b12237f8d2370735dbd30566c46ad26dc0a
Albert Zak <me@albertzak.com>
2017-02-06 14:18:45 +0100

    🃏 Add check for mobile numbers

---
+++ app/meteor/api/messages/methods/isMobileNumber.test.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+import { expect } from 'chai'
+import { isMobileNumber } from './isMobileNumber'
+
+describe('api', () => {
+  describe('messages', () => {
+    describe('isMobileNumber', () => {
+      context('accepts valid number', () => {
+        it('without country code', () => {
+          expect(isMobileNumber('0660 1234567')).to.equal(true)
+        })
+
+        it('without leading zero', () => {
+          expect(isMobileNumber('660 1234567')).to.equal(true)
+        })
+
+        it('with country code', () => {
+          expect(isMobileNumber('+43 660 1234567')).to.equal(true)
+        })
+      })
+
+      context('rejects invalid number', () => {
+        it('when too short', () => {
+          expect(isMobileNumber('66012')).to.not.equal(true)
+        })
+
+        it('without country code', () => {
+          expect(isMobileNumber('01234567')).to.not.equal(true)
+        })
+
+        it('with other country codes', () => {
+          expect(isMobileNumber('+42660123')).to.not.equal(true)
+        })
+
+        it('without leading zero', () => {
+          expect(isMobileNumber('1234567')).to.not.equal(true)
+        })
+
+        it('with country code', () => {
+          expect(isMobileNumber('+43 1234567')).to.not.equal(true)
+        })
+      })
+    })
+  })
+})
