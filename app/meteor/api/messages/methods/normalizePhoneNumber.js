475f0a10a727327b984fe8eec9551c72fdb55209
Albert Zak <me@albertzak.com>
2017-02-13 00:00:02 +0100

    ☎️ Add method to normalize phone numbers to msisdn format

---
+++ app/meteor/api/messages/methods/normalizePhoneNumber.js
@@ -0,0 +1,40 @@
+import flow from 'lodash/flow'
+
+const countryPrefix = '43'
+const minimumLength = 9
+
+export const checkArgument = (s) => {
+  if (typeof s === 'string') {
+    return s
+  } else {
+    throw new Error('Missing argument of type string')
+  }
+}
+
+export const stripNonNumber = (s) => s.replace(/[^0-9]*/g, '')
+
+export const stripLeadingZeroes = (s) => s.replace(/^0*/, '')
+
+export const addCountryCode = (s) => {
+  if (s.indexOf(countryPrefix) === 0) {
+    return s
+  } else {
+    return countryPrefix + s
+  }
+}
+
+export const ensureMinimumLength = (s) => {
+  if (s.length >= minimumLength) {
+    return s
+  } else {
+    throw new Error(`Number is shorter than the minimum length ${minimumLength}`)
+  }
+}
+
+export const normalizePhoneNumber = flow(
+  checkArgument,
+  stripNonNumber,
+  stripLeadingZeroes,
+  addCountryCode,
+  ensureMinimumLength
+)
---
+++ app/meteor/api/messages/methods/normalizePhoneNumber.test.js
@@ -0,0 +1,31 @@
+/* eslint-env mocha */
+import { expect } from 'chai'
+import { stripLeadingZeroes, normalizePhoneNumber } from './normalizePhoneNumber'
+
+describe('api', () => {
+  describe('messages', () => {
+    describe('normalizePhoneNumber', () => {
+      const normalized = '436601234567'
+
+      it('without country code', () => {
+        expect(normalizePhoneNumber('0660 1234567')).to.equal(normalized)
+      })
+
+      it('without leading zero', () => {
+        expect(normalizePhoneNumber('660 1234567')).to.equal(normalized)
+      })
+
+      it('with country code and without leading zero', () => {
+        expect(normalizePhoneNumber('+43 660 1234567')).to.equal(normalized)
+      })
+
+      it('with leading zeroes and country code', () => {
+        expect(normalizePhoneNumber('0043 660 1234567')).to.equal(normalized)
+      })
+
+      it('strips leading zeroes', () => {
+        expect(stripLeadingZeroes('0660')).to.equal('660')
+      })
+    })
+  })
+})
