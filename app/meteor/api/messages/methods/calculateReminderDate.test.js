2b67978d8ca6126a1512a009fb08b397521f4df2
Albert Zak <me@albertzak.com>
2017-02-21 11:14:08 +0100

    🎖 Add methods to skip n days excluding weekends

---
+++ app/meteor/api/messages/methods/calculateReminderDate.test.js
@@ -0,0 +1,64 @@
+/* eslint-env mocha */
+import chai from 'chai'
+import chaiDatetime from 'chai-datetime'
+import moment from 'moment'
+import { calculateReminderDate, calculateFutureCutoff } from './calculateReminderDate'
+
+chai.use(chaiDatetime)
+const expect = chai.expect
+
+describe('api', () => {
+  describe('messages', () => {
+    describe('calculateReminderDate', () => {
+      context('counts backwards two working days', () => {
+        it('counts continuous weekdays', () => {
+          const fri = moment('2017-02-23T10:15:00').toDate()
+          const wed = moment('2017-02-21T10:15:00').toDate()
+          const daysBefore = 2
+
+          const reminder = calculateReminderDate(fri, daysBefore).toDate()
+          expect(reminder).to.equalDate(wed)
+        })
+
+        it('skips weekends by default', () => {
+          const mon = moment('2017-02-20T10:15:00').toDate()
+          const thuBefore = moment('2017-02-16T10:15:00').toDate()
+          const daysBefore = 2
+
+          const reminder = calculateReminderDate(mon, daysBefore).toDate()
+          expect(reminder).to.equalDate(thuBefore)
+        })
+
+        it('skips whole month', () => {
+          const mon = moment('2017-02-20T10:15:00').toDate()
+          const lastJanuary = moment('2017-01-30T10:15:00').toDate()
+          const skipDay = (d) => d.month() === 1
+          const daysBefore = 2
+
+          const reminder = calculateReminderDate(mon, daysBefore, skipDay).toDate()
+          expect(reminder).to.equalDate(lastJanuary)
+        })
+
+        it('skips whole month and weekends', () => {
+          const mon = moment('2017-02-20T10:15:00').toDate()
+          const lastJanuaryBeforeWeekend = moment('2017-01-27T10:15:00').toDate()
+          const skipDay = (d) => (d.month() === 1 || d.isoWeekday() === 6 || d.isoWeekday() === 7)
+          const daysBefore = 3
+
+          const reminder = calculateReminderDate(mon, daysBefore, skipDay).toDate()
+          expect(reminder).to.equalDate(lastJanuaryBeforeWeekend)
+        })
+
+        it('skip in forward direction', () => {
+          const lastJanuaryBeforeWeekend = moment('2017-01-27T10:15:00').toDate()
+          const firstMarch = moment('2017-03-01T10:15:00').toDate()
+          const skipDay = (d) => (d.month() === 1 || d.isoWeekday() === 6 || d.isoWeekday() === 7)
+          const daysBefore = 3
+
+          const reminder = calculateFutureCutoff(lastJanuaryBeforeWeekend, daysBefore, skipDay).toDate()
+          expect(reminder).to.equalDate(firstMarch)
+        })
+      })
+    })
+  })
+})
