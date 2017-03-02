38197fabbf50b8481bff5b01689e9a4f402b210d
Albert Zak <me@albertzak.com>
2017-03-02 11:25:14 +0100

    🏅 Hide no-show indicator for canceled appointments

--- app/meteor/client/ui/appointments/appointment/Indicator.jsx
+++ app/meteor/client/ui/appointments/appointment/Indicator.jsx
@@ -4,16 +4,17 @@ import { TAPi18n } from 'meteor/tap:i18n'
 import { Icon } from 'client/ui/components/Icon'
 
 export const Indicator = ({ appointment }) => (
-  <span className="pull-right">
-    {
-      (appointment.treated || appointment.admitted)
-        ? (<span
-          key="show"
-          title={TAPi18n.__('appointments.show')}
-          style={{ display: 'inline-block', color: '#8fc6ae' }}>
-          <Icon name="check" />&nbsp;
-        </span>
-      ) : ((moment().diff(appointment.end, 'minutes') >= 90) &&
+  appointment.patientId && !appointment.canceled &&
+    <span className="pull-right">
+      {
+        (appointment.treated || appointment.admitted)
+          ? (<span
+            key="show"
+            title={TAPi18n.__('appointments.show')}
+            style={{ display: 'inline-block', color: '#8fc6ae' }}>
+            <Icon name="check" />&nbsp;
+          </span>
+        ) : ((moment().diff(appointment.end, 'minutes') >= 90) &&
           <span
             key="noShow"
             title={TAPi18n.__('appointments.noShow')}
@@ -21,6 +22,6 @@ export const Indicator = ({ appointment }) => (
             <Icon name="times" />&nbsp;
           </span>
         )
-    }
-  </span>
+      }
+    </span> || null
 )
