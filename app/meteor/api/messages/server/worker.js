88ce35b62d297ed8cb3de96556989e812bb1192d
Albert Zak <me@albertzak.com>
2017-03-02 02:44:07 +0100

    🛫 Revert timeout before failing job

--- app/meteor/api/messages/server/worker.js
+++ app/meteor/api/messages/server/worker.js
@@ -1,6 +1,5 @@
 import once from 'lodash/once'
 import moment from 'moment'
-import { Meteor } from 'meteor/meteor'
 import { Job } from 'meteor/vsivsi:job-collection'
 import { Messages } from 'api/messages'
 
@@ -27,7 +26,7 @@ export const worker = (job, callback) => {
     Messages.actions.sendScheduled.callPromise()
   ]).catch((e) => {
     console.error('[Messages] worker: errored with', e)
-    Meteor.setTimeout(() => job.fail(), 5000)
+    job.fail()
   }).then(() => {
     cleanOldJobs(job)
     job.done()
