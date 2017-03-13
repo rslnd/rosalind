3e29d2875e9362d6d702c4bd241d91c737a06cfb
Albert Zak <me@albertzak.com>
2017-03-13 20:18:02 +0100

    🖥 Clear screen on startup

--- app/meteor/startup/server/banner.js
+++ app/meteor/startup/server/banner.js
@@ -1,27 +1,39 @@
 import { Meteor } from 'meteor/meteor'
 
 export const BANNER = `
-
               .        .
       ,-. ,-. |  ,-. ,-|
       |   \`-. |  | | | |
       '   \`-' \`' ' ' \`-'
-
-
 `
 
 export default () => {
+  const id = (i) => i
+
   const info = [
     process.env.CUSTOMER_NAME,
-    process.env.TZ_CLIENT,
-    Meteor.release,
-    process.env.COMMIT_HASH,
-    process.env.NODE_ENV,
-    process.env.TEST,
-    process.env.BUILD_NUMBER
+    [
+      Meteor.release,
+      process.env.NODE_ENV,
+      process.env.TZ_CLIENT
+    ].filter(id).join(' '),
+    [
+      (process.env.TEST && 'TEST'),
+      process.env.BUILD_NUMBER,
+      process.env.COMMIT_HASH
+    ].filter(id).join(' ')
   ]
 
+  // Clear screen
+  if (process.env.NODE_ENV === 'development' && !process.env.CI) {
+    console.log('\x1Bc')
+  }
+
   console.log(BANNER)
-  console.log(info.filter((i) => i).map((i) => `     ${i}`).join('\n'))
+  console.log(info
+    .filter(id)
+    .filter((s) => s.length > 0)
+    .map((i) => `     ${i}`)
+    .join('\n'))
   console.log('\n')
 }
