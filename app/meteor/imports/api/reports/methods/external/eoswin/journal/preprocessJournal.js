c3017d0585b0a3dec51696b3be47d589fc25d31b
Albert Zak <me@albertzak.com>
2017-10-02 20:21:36 +0200

    🚝 Count multiple surgeries/cauteries/cryos multiple times

--- app/meteor/imports/api/reports/methods/external/eoswin/journal/preprocessJournal.js
+++ app/meteor/imports/api/reports/methods/external/eoswin/journal/preprocessJournal.js
@@ -1,6 +1,5 @@
 import map from 'lodash/map'
 import mapValues from 'lodash/fp/mapValues'
-import add from 'lodash/fp/add'
 import groupBy from 'lodash/fp/groupBy'
 import identity from 'lodash/fp/identity'
 import { isNew, isSurgery, isCautery, isCryo, isAny } from '../insuranceCodes'
@@ -24,9 +23,9 @@ const parseAppointmentType = (row) => {
   return {
     isAdmitted: isAny(codes) || isMissingReimbursement(row.Text),
     isNew: isNew(codes),
-    isSurgery: isSurgery(codes),
-    isCautery: isCautery(codes),
-    isCryo: isCryo(codes),
+    surgery: codes.filter(c => isSurgery([c])).length,
+    cautery: codes.filter(c => isCautery([c])).length,
+    cryo: codes.filter(c => isCryo([c])).length,
     isMissingReimbursement: isMissingReimbursement(row.Text),
     assignee: row.Assignee
   }
@@ -43,14 +42,16 @@ const isMissingReimbursement = (text) =>
 const sumRows = (acc, curr, i) => {
   const total = incrementIf(curr.isAdmitted)(acc.total)
   const _new = incrementIf(curr.isNew)(acc.new)
-  const surgery = incrementIf(curr.isSurgery)(acc.surgery)
-  const cautery = incrementIf(curr.isCautery)(acc.cautery)
-  const cryo = incrementIf(curr.isCryo)(acc.cryo)
+  const surgery = add(curr.surgery, acc.surgery)
+  const cautery = add(curr.cautery, acc.cautery)
+  const cryo = add(curr.cryo, acc.cryo)
   const missingReimbursement = incrementIf(curr.isMissingReimbursement)(acc.missingReimbursement)
   const recall = total - _new
 
   return { total, new: _new, recall, surgery, cautery, cryo, missingReimbursement }
 }
+
+const add = (a = 0, b = 0) => a + b
 
 export const incrementIf = condition => value =>
   condition ? ((parseInt(value) || 0) + 1) : (parseInt(value) || 0)
