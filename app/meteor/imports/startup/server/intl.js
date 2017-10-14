32bec082b0ac21838b179fcd6f0293822f0b545b
Albert Zak <me@albertzak.com>
2017-10-14 22:11:22 +0200

    🏃 Remove icu logging on startup

--- app/meteor/imports/startup/server/intl.js
+++ app/meteor/imports/startup/server/intl.js
@@ -1,9 +1,6 @@
-import icu from 'full-icu'
 import IntlPolyfill from 'intl'
 
 export default () => {
   Intl.NumberFormat = IntlPolyfill.NumberFormat
   Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
-
-  console.log(icu)
 }
