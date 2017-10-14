efb5c9ef95dc36cb93c469a0b4c923b21a46fd88
Albert Zak <me@albertzak.com>
2017-10-14 12:37:07 +0200

    🐏 Add full-icu package and debug output on startup

--- app/meteor/imports/startup/server/intl.js
+++ app/meteor/imports/startup/server/intl.js
@@ -1,6 +1,9 @@
+import icu from 'full-icu'
 import IntlPolyfill from 'intl'
 
 export default () => {
   Intl.NumberFormat = IntlPolyfill.NumberFormat
   Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
+
+  console.log(icu)
 }
