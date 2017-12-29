c1a2c46d51a29076d2fb83413f220fa4f14f8a08
Albert Zak <me@albertzak.com>
2017-12-29 21:43:21 +0100

    🍔 Group consecutive comments from same author

--- app/meteor/imports/client/comments/CommentItem.js
+++ app/meteor/imports/client/comments/CommentItem.js
@@ -3,17 +3,39 @@ import { Avatar } from '../users/Avatar'
 import { UserHelper } from '../users/UserHelper'
 import { RelativeTime } from '../helpers/RelativeTime'
 
+const childCommentStyle = {
+  paddingTop: 4,
+  paddingBottom: 4,
+  borderTop: '1px solid #eee'
+}
+
+const ChildCommentItem = ({ comment }) => (
+  <div style={childCommentStyle}>
+    <span className='text-muted pull-right'><RelativeTime time={comment.createdAt} /></span>
+    {comment.body}
+  </div>
+)
+
 export class CommentItem extends React.Component {
   render () {
+    const { createdBy, createdAt, body, children } = this.props.comment
+
     return (
       <div className='box-comment'>
-        <div><Avatar userId={this.props.comment.createdBy} /></div>
+        <div><Avatar userId={createdBy} /></div>
         <div className='comment-text'>
           <span className='username'>
-            <span><UserHelper userId={this.props.comment.createdBy} helper='fullName' /></span>
-            <span className='text-muted pull-right'><RelativeTime time={this.props.comment.createdAt} /></span>
+            <span><UserHelper userId={createdBy} helper='fullName' /></span>
+            <span className='text-muted pull-right'><RelativeTime time={createdAt} /></span>
           </span>
-          <p className='enable-select break-word'>{this.props.comment.body}</p>
+          <p className='enable-select break-word'>
+            {body}
+            {
+              children && children.map(c =>
+                <ChildCommentItem key={c._id} comment={c} />
+              )
+            }
+          </p>
         </div>
       </div>
     )
--- app/meteor/imports/client/comments/CommentsList.js
+++ app/meteor/imports/client/comments/CommentsList.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import concat from 'lodash/concat'
+import reverse from 'lodash/reverse'
 import { CommentItem } from './CommentItem'
+
+const accumulateBy = field => list =>
+  list.reduce((acc, curr) => {
+    const lastGroup = acc.lastGroup
+    const currGroup = curr[field]
+
+    if (currGroup === lastGroup) {
+      // append to last item
+      const [last, ...rest] = reverse(acc.stack)
+      const appended = {
+        ...last,
+        children: concat(last.children || [], curr)
+      }
+      const newStack = [...reverse(rest), appended]
+      return { lastGroup: currGroup, stack: newStack }
+    } else {
+      // push new
+      return { lastGroup: currGroup, stack: [...acc.stack, curr] }
+    }
+  }, { lastGroup: null, stack: [] }).stack
 
 export const CommentsList = ({ comments }) => {
   if (comments.length === 0) {
@@ -7,7 +29,7 @@ export const CommentsList = ({ comments }) => {
   } else {
     return (
       <div className='box-footer box-comments'>
-        {comments.map((comment) => {
+        {accumulateBy('createdBy')(comments).map((comment) => {
           return <CommentItem key={comment._id} comment={comment} />
         })}
       </div>
