660a0ed45bf5922789ab006ca61536f9346dc9d1
Albert Zak <me@albertzak.com>
2018-01-15 03:00:10 +0100

    Revert "🌰 Replace appointments search with patient picker"
    This reverts commit 7384d68a2305b94515a046b25b946366dbb5ac49.

--- app/meteor/imports/client/appointments/reducers.js
+++ app/meteor/imports/client/appointments/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
+import { search } from './search/reducers'
 import { move } from './dayView/reducers'
 
 export default combineReducers({
+  search,
   move
 })
--- app/meteor/imports/client/patients/reducers.js
+++ app/meteor/imports/client/patients/reducers.js
@@ -4,15 +4,6 @@ export const loadPatient = (state = {}, action) => {
       return {
         data: action.data
       }
-
-    case 'DATA_TRANSFER_SUCCESS':
-      if (action.importer === 'xdt') {
-        return {
-          data: action.result
-        }
-      }
-      return state
-
     default:
       return state
   }
