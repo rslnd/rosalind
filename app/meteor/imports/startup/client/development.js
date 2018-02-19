514f3b48e33ad1c7216c47ed3bc0232487a84584
Albert Zak <me@albertzak.com>
2018-02-19 22:50:34 +0100

    🤔 Add why-did-you-update

--- app/meteor/imports/startup/client/development.js
+++ app/meteor/imports/startup/client/development.js
@@ -1,8 +1,15 @@
+import React from 'react'
 import { process as server } from 'meteor/clinical:env'
 import * as Api from '../../api'
 
 export default () => {
   if (server.env.NODE_ENV !== 'production') {
     window.Api = Api
+
+    // Add ?why to the URL to blast the console when React is making unnecessary updates
+    if (window.location.toString().indexOf('why') !== -1) {
+      const { whyDidYouUpdate } = require('why-did-you-update')
+      whyDidYouUpdate(React)
+    }
   }
 }
