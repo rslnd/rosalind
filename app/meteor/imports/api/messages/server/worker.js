35b138683a04f39da760f8354e567bf75d3ca228
Albert Zak <me@albertzak.com>
2018-02-21 23:59:52 +0100

    🍶 Reenable workers

--- app/meteor/imports/api/messages/server/worker.js
+++ app/meteor/imports/api/messages/server/worker.js
@@ -17,18 +17,18 @@ export const worker = (job, callback) => {
   // We can't check for quiet time here, because doing so would
   // postpone the cancelation confirmation until next morning
   // when a patient wants to cancel her appointment at night
-  // try {
-  //   Messages.actions.createReminders.callPromise()
-  //   .then(() => Messages.actions.sendScheduled.callPromise())
-  //   .then(() => {
-  //     cleanOldJobs(job)
-  //     job.done()
-  //     callback()
-  //   }).catch((e) => {
-  //     console.error('[Messages] worker: errored with', e)
-  //     job.fail()
-  //   })
-  // } catch (e) {
-  //   console.error('[Messages] worker: hard errored with', e)
-  // }
+  try {
+    Messages.actions.createReminders.callPromise()
+    .then(() => Messages.actions.sendScheduled.callPromise())
+    .then(() => {
+      cleanOldJobs(job)
+      job.done()
+      callback()
+    }).catch((e) => {
+      console.error('[Messages] worker: errored with', e)
+      job.fail()
+    })
+  } catch (e) {
+    console.error('[Messages] worker: hard errored with', e)
+  }
 }
