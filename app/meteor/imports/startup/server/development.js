792b7d9e63f66b56872ac617cded791c589dcddf
Albert Zak <me@albertzak.com>
2018-05-17 00:28:53 +0200

    🐍 Add ENABLE_TRACE flag

--- app/meteor/imports/startup/server/development.js
+++ app/meteor/imports/startup/server/development.js
@@ -1,6 +1,16 @@
 import * as Api from '../../api'
+import Fiber from 'fibers'
 
 export default () => {
+  if (process.env.ENABLE_TRACE) {
+    setInterval(() => {
+      const { rss, heapTotal, heapUsed } = process.memoryUsage()
+      const { fibersCreated, poolSize } = Fiber
+
+      console.log({ rss, heapTotal, heapUsed, fibersCreated, poolSize })
+    }, 5000).unref()
+  }
+
   if (process.env.NODE_ENV === 'production') { return }
 
   global.Api = Api
