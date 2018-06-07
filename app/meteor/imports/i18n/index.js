6d4f16234d6a902966300ac18d0c071b08cacce6
Albert Zak <me@albertzak.com>
2018-06-07 23:58:44 +0200

    🎷 I don't know how to pluralize word

--- app/meteor/imports/i18n/index.js
+++ app/meteor/imports/i18n/index.js
@@ -3,7 +3,9 @@ import * as de from './de'
 
 const translations = dot.flatten(de)['$set'] // Sorry...
 
-export const __ = (key, substitutions) => {
+export const __ = (originalKey, substitutions) => {
+  const key = pluralizeKey(originalKey, substitutions)
+
   const translation = translations[key]
 
   if (!translation) {
@@ -11,6 +13,10 @@ export const __ = (key, substitutions) => {
     return key
   }
 
+  return substitute(translation, substitutions) || key
+}
+
+const substitute = (translation, substitutions) => {
   if (substitutions && Object.keys(substitutions) !== []) {
     const search = Object.keys(substitutions)
 
@@ -18,11 +24,18 @@ export const __ = (key, substitutions) => {
       return t.replace('__' + s + '__', substitutions[s])
     }, translation)
 
-    return replaced || key
+    return replaced || translation
+  } else {
+    return translation
   }
-
-  return translation || key
 }
+
+const isPlural = n => ((n >= 2) || (n <= -2) || (n === 0))
+
+const pluralizeKey = (key, substitutions) =>
+  (substitutions && isPlural(substitutions.count))
+  ? `${key}_plural`
+  : key
 
 if (Meteor.isClient) {
   Template.registerHelper('_', __)
