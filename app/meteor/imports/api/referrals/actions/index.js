3a231aabe00289f34f77e607946d70d0af70433e
Albert Zak <me@albertzak.com>
2018-06-10 15:40:38 +0200

    🕵 Add referrals detail method

--- app/meteor/imports/api/referrals/actions/index.js
+++ app/meteor/imports/api/referrals/actions/index.js
@@ -1,9 +1,11 @@
 import { insert } from './insert'
+import { detail } from './detail'
 import { tally } from './tally'
 
 export default function ({ Referrals }) {
   return {
     insert: insert({ Referrals }),
+    detail: detail({ Referrals }),
     tally: tally({ Referrals })
   }
 }
