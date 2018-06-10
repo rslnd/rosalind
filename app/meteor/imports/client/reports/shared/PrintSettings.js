093094936f62692efc91fc88c73a18b58a000160
Albert Zak <me@albertzak.com>
2018-06-10 15:41:15 +0200

    👏 Make print page orientation declarative

---
+++ app/meteor/imports/client/reports/shared/PrintSettings.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const PrintSettings = ({ orientation }) => {
+  if (orientation !== 'landscape' && orientation !== 'portrait') {
+    console.error('[PrintSettings] Invalid orientation', orientation)
+  }
+
+  const style = `
+    @media print {
+      @page { size: ${orientation}; }
+    }
+  `
+
+  return (
+    <style dangerouslySetInnerHTML={{__html: style}} />
+  )
+}
