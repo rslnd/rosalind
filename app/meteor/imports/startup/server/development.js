78c4ec8172fc1ff42f4cd6704993f87e198e900b
Albert Zak <me@albertzak.com>
2018-06-12 18:50:24 +0200

    🌶 Add logging of mongo events

--- app/meteor/imports/startup/server/development.js
+++ app/meteor/imports/startup/server/development.js
@@ -20,6 +20,27 @@ export default () => {
   //   }, 20 * 1000).unref()
   // }
 
+  const Mongo = MongoInternals.defaultRemoteCollectionDriver().mongo
+  const db = Mongo.db
+  const multiplexers = Mongo._observeMultiplexers
+
+  db.s.topology.on('close', e => {
+    console.error('[Debug] Topology close')
+    console.log(`Number of observes are active: ${Object.keys(multiplexers).length}`)
+  })
+
+  db.s.topology.on('reconnect', data =>
+    console.log('[Debug] Successfully reconnected to mongo'))
+
+  db.s.topology.on('reconnectFailed', data =>
+    console.error('[Debug] Failed to reconnect to mongo'))
+
+  db.s.topology.on('left', data =>
+    console.log('[Debug] A server left the replica set'))
+
+  db.s.topology.on('joined', data =>
+    console.log('[Debug] A server joined the replica set'))
+
   if (process.env.NODE_ENV === 'production') { return }
 
   global.Api = Api
