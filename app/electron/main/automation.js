b95f9e40856b32b0f3f9fc77bb7e486206b9e977
Albert Zak <me@albertzak.com>
2018-07-03 19:44:27 +0200

    😌 Revert to extracting exe and streaming logs with spawn

--- app/electron/main/automation.js
+++ app/electron/main/automation.js
@@ -1,6 +1,10 @@
 const childProcess = require('child_process')
+const fs = require('fs')
 const path = require('path')
+const temp = require('temp')
 const logger = require('./logger')
+
+const exeName = 'generateEoswinReports.exe'
 
 const start = (argv = []) => {
   if (argv.join(' ').indexOf('generateEoswinReports') !== -1) {
@@ -9,25 +13,56 @@ const start = (argv = []) => {
 }
 
 const generateEoswinReports = () => {
-  const exePath = path.join(__dirname, '..', 'generateEoswinReports.exe')
-  logger.info('[automation] Spawning', exePath)
-
-  // Need to use execFile here because it is the only child process
-  // function that transparently extracts the executable from an asar package
-  return childProcess.execFile(exePath, [], {
-    shell: true
-  }, (err, stdout, stderr) => {
+  extractExe((err, exePath) => {
     if (err) {
-      logger.error('[automation] generateEoswinReports error:', err)
+      logger.error('[automation] Failed to extract exe', err)
+      return
     }
 
-    if (stdout) {
-      logger.info('[automation] generateEoswinReports', stdout)
-    }
+    logger.info('[automation] Spawning', exePath)
 
-    if (stderr) {
-      logger.info('[automation] generateEoswinReports stderr', stderr)
-    }
+    const child = childProcess.spawn(exePath)
+    child.stdout.setEncoding('utf8')
+
+    child.stdout.on('data', d =>
+      logger.info('[automation] generateEoswinReports', d)
+    )
+
+    child.stderr.on('data', d =>
+      logger.error('[automation] generateEoswinReports error:', d)
+    )
+
+    return new Promise((resolve, reject) => {
+      child.on('close', code => {
+        logger.info('[automation] generateEoswinReports exited with code', code)
+
+        if (code !== 0) {
+          logger.error('[automation] generateEoswinReports failed')
+          reject(code)
+        } else {
+          resolve()
+        }
+      })
+    })
+  })
+}
+
+const extractExe = (cb) => {
+  temp.track()
+
+  temp.mkdir('rosalind', (err, tmpDir) => {
+    if (err) { return cb(err) }
+    const asarPath = path.join(__dirname, '..', exeName)
+    const tempPath = path.join(tmpDir, exeName)
+
+    const read = fs.createReadStream(asarPath)
+    const write = fs.createWriteStream(tempPath)
+
+    read.on('error', e => cb(e))
+    write.on('error', e => cb(e))
+    write.on('close', e => cb(null, tempPath))
+
+    read.pipe(write)
   })
 }
 
