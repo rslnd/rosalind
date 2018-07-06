6516d87afb490d5be8c43c1714cc11ef46ec5ec8
Albert Zak <me@albertzak.com>
2018-07-06 16:38:04 +0200

    🐔 Display optional note over blocked times

--- app/meteor/imports/client/appointments/dayView/grid/schedules.js
+++ app/meteor/imports/client/appointments/dayView/grid/schedules.js
@@ -30,13 +30,14 @@ export const schedules = ({ assignees, onDoubleClick, slotSize }) => (
           assigneeId={s.userId}
           start={s.start}
           end={s.end}
+          note={s.note}
           slotSize={slotSize}
           onDoubleClick={onDoubleClick} />
       )
   ))
 )
 
-const Schedule = ({ start, end, scheduleId, assigneeId, slotSize, onDoubleClick }) => {
+const Schedule = ({ start, end, note, scheduleId, assigneeId, slotSize, onDoubleClick }) => {
   const timeStart = moment(start).floor(5, 'minutes')
   const timeEnd = moment(end).ceil(5, 'minutes')
   const duration = (end - start) / 1000 / 60
@@ -53,16 +54,20 @@ const Schedule = ({ start, end, scheduleId, assigneeId, slotSize, onDoubleClick 
       }}>
 
       {
-        duration > slotSize
+        (duration - (note ? slotSize : 0)) > slotSize
         ? [
           <div key={1} style={style.schedulesText}>
             {!isFirstSlot(timeStart) && timeStart.format('H:mm')}
           </div>,
-          <div key={2} style={style.schedulesText}>
+          note ? <div key={2} style={style.schedulesText}>
+            {note}
+          </div> : null,
+          <div key={3} style={style.schedulesText}>
             {!isLastSlot(timeEnd) && timeEnd.format('H:mm')}
           </div>
         ] : <div style={style.schedulesText}>
           {timeStart.format('H:mm')} - {timeEnd.format('H:mm')}
+          {note && <span> {note}</span>}
         </div>
       }
     </div>
