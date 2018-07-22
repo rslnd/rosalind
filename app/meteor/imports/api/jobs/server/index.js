44ee9c6a17fcbeb4b2b24d898199c84f98b638a4
Albert Zak <me@albertzak.com>
2018-07-22 22:49:30 +0200

    🐠 Remove tabular job collection

--- app/meteor/imports/api/jobs/server/index.js
+++ app/meteor/imports/api/jobs/server/index.js
@@ -1,10 +1,8 @@
 import logging from './logging'
-import security from './security'
 import worker from './worker'
 import '../table'
 
 export default function () {
   logging()
-  security()
   worker()
 }
