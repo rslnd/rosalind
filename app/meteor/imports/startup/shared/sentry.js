8a77d13537670c5c2779593da3547553ee447762
Albert Zak <me@albertzak.com>
2018-07-30 22:17:43 +0200

    👩‍👩‍👧 Run sentry in production only

--- app/meteor/imports/startup/shared/sentry.js
+++ app/meteor/imports/startup/shared/sentry.js
@@ -5,15 +5,21 @@ const env = Meteor.isClient
   ? require('meteor/clinical:env').process.env
   : process.env
 
-if (!env.SENTRY_DSN_URL_PUBLIC) {
-  console.warn('[Sentry] Please set SENTRY_DSN_URL_PUBLIC')
+let sentry = {}
+
+if (process.env.NODE_ENV === 'production') {
+  if (!env.SENTRY_DSN_URL_PUBLIC) {
+    console.warn('[Sentry] Please set SENTRY_DSN_URL_PUBLIC')
+  }
+
+  sentry = new RavenLogger({
+    publicDSN: env.SENTRY_DSN_URL_PUBLIC,
+    privateDSN: env.SENTRY_DSN_URL_PRIVATE,
+    shouldCatchConsoleError: true,
+    trackUser: false
+  }, {
+    release: env.COMMIT_HASH
+  })
 }
 
-export const sentry = new RavenLogger({
-  publicDSN: env.SENTRY_DSN_URL_PUBLIC,
-  privateDSN: env.SENTRY_DSN_URL_PRIVATE,
-  shouldCatchConsoleError: true,
-  trackUser: false
-}, {
-  release: env.COMMIT_HASH
-})
+export { sentry }
