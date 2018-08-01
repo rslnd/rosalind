7ab7eac06a5485b513c99410cfc8b71a32b3b6b7
Albert Zak <me@albertzak.com>
2018-08-01 17:10:39 +0200

    😹 Fix settings

--- app/meteor/imports/client/system/settings/SettingsContainer.js
+++ app/meteor/imports/client/system/settings/SettingsContainer.js
@@ -4,17 +4,17 @@ import { SettingsScreen } from './SettingsScreen'
 import { subscribe } from '../../../util/meteor/subscribe'
 
 export const composer = (props) => {
-  subscribe('settings').ready()
+  const isLoading = !subscribe('settings').ready()
 
   const get = (key) => Settings.get(key)
   const set = (key, value) => Settings.actions.set.call({ key, value })
 
-  const settings = Settings.find({})
+  const settings = Settings.find({}).fetch()
 
   settings.get = get
   settings.set = set
 
-  return { get, set, settings }
+  return { isLoading, get, set, settings }
 }
 
 export const SettingsContainer = withTracker(composer)(SettingsScreen)
