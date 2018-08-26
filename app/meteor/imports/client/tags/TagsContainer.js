9159dfb6bac7949f1a0254980742b618244a2f9c
Albert Zak <me@albertzak.com>
2018-08-26 12:31:28 +0200

    🍟 Sort tags by calendar order

--- app/meteor/imports/client/tags/TagsContainer.js
+++ app/meteor/imports/client/tags/TagsContainer.js
@@ -1,15 +1,19 @@
+import sortBy from 'lodash/fp/sortBy'
 import { withTracker } from '../components/withTracker'
 import { Tags } from '../../api/tags'
 import { Users } from '../../api/users'
 import { Calendars } from '../../api/calendars'
-import { Loading } from '../components/Loading'
 import { TagsScreen } from './TagsScreen'
 
-const composer = (props) => {
-  const tags = Tags.find({}, { sort: { order: 1 } }).fetch()
+const sortByCalendar = sortBy(({ calendarIds }) =>
+  (Calendars.findOne({ _id: { $in: calendarIds }}, { sortBy: { order: 1 } }) || {}).order
+)
 
-  const getCalendarName = id => id && Calendars.findOne(id) && Calendars.findOne(id).name
-  const getAssigneeName = id => id && Users.methods.fullNameWithTitle(Users.findOne(id))
+const composer = (props) => {
+  const tags = sortByCalendar(Tags.find({}, { sort: { order: 1 } }).fetch())
+
+  const getCalendarName = _id => _id && ((Calendars.findOne({ _id }) || {}).name)
+  const getAssigneeName = _id => _id && Users.methods.fullNameWithTitle(Users.findOne({ _id }))
   const handleUpdate = (_id, update) => {
     Tags.update({ _id }, update)
   }
