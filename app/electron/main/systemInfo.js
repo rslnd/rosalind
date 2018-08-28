db65a9f48939f68422c5676d5cd329a092e8adc2
Albert Zak <me@albertzak.com>
2018-08-28 20:35:52 +0200

    🍱 Send hostname etc with systeminfo

--- app/electron/main/systemInfo.js
+++ app/electron/main/systemInfo.js
@@ -1,7 +1,14 @@
+const { app } = require('electron')
+const os = require('os')
+
 const systemInfo = {
   send: ({ ipcReceiver }) => {
     const info = {
-      client: 'electron'
+      client: 'electron',
+      version: app.getVersion(),
+      hostname: os.hostname(),
+      username: os.userInfo().username,
+      os: os.release()
     }
 
     ipcReceiver.webContents.send('systemInfo', info)
