e416fbdaa37e56d2fcbe2723a08795110de49a95
Albert Zak <me@albertzak.com>
2018-10-16 22:09:56 +0200

    🏝 More perfomant namecase

--- app/meteor/imports/util/namecase.js
+++ app/meteor/imports/util/namecase.js
@@ -1,7 +1,10 @@
 import nc from 'namecase'
 
+const checkName = a =>
+  a === a.toUpperCase() || a === a.toLowerCase()
+
 export const namecase = (a = '') => {
-  if (nc.checkName(a)) {
+  if (checkName(a)) {
     return nc(a)
   } else {
     return a
