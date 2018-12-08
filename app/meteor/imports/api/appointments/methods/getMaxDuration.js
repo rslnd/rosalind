bdfad2060d27b572fde3e2413f222afeaa822ff7
Albert Zak <me@albertzak.com>
2018-12-08 20:52:08 +0100

    ⚾ Fix maxDuration to include next override schedule

--- app/meteor/imports/api/appointments/methods/getMaxDuration.js
+++ app/meteor/imports/api/appointments/methods/getMaxDuration.js
@@ -1,10 +1,21 @@
 import momentTz from 'moment-timezone'
 import { extendMoment } from 'moment-range'
+import { Schedules } from '../../schedules'
 
 const moment = extendMoment(momentTz)
 
 export default ({ Appointments }) => {
   const getMaxDuration = ({ time, assigneeId, calendarId }) => {
+    const maxDuration = Math.min(
+      untilNextAppointment({ time, assigneeId, calendarId }),
+      untilNextOverride({ time, assigneeId, calendarId })
+    )
+
+    // TODO: Make recipient deal with Infinity
+    return maxDuration === Infinity ? null : maxDuration
+  }
+
+  const untilNextAppointment = ({ time, assigneeId, calendarId }) => {
     const nextAppointments = Appointments.find({
       start: { $gt: time },
       assigneeId: assigneeId,
@@ -19,7 +30,27 @@ export default ({ Appointments }) => {
         ).diff('minutes')
       )
     } else {
-      return null
+      return Infinity
+    }
+  }
+
+  const untilNextOverride = ({ time, assigneeId, calendarId }) => {
+    const nextOverrides = Schedules.find({
+      type: 'override',
+      start: { $gt: time },
+      userId: assigneeId,
+      calendarId: calendarId
+    }, { sort: { start: 1 }, limit: 1 }).fetch()
+
+    if (nextOverrides[0]) {
+      return Math.abs(
+        moment.range(
+          time,
+          nextOverrides[0].start
+        ).diff('minutes')
+      )
+    } else {
+      return Infinity
     }
   }
 
