7585ef8d080fe8e3004dcf2db59839310bb4f0e4
Albert Zak <me@albertzak.com>
2018-12-10 11:39:32 +0100

    🐠 Fix i18n in tags screen

--- app/meteor/imports/client/tags/TagsContainer.js
+++ app/meteor/imports/client/tags/TagsContainer.js
@@ -5,6 +5,7 @@ import { Tags } from '../../api/tags'
 import { Users } from '../../api/users'
 import { Calendars } from '../../api/calendars'
 import { TagsScreen } from './TagsScreen'
+import { __ } from '../../i18n'
 
 const sortByCalendar = sortBy(({ calendarIds = [] }) =>
   (Calendars.findOne({ _id: { $in: calendarIds } }, { sortBy: { order: 1 } }) || {}).order
@@ -17,15 +18,15 @@ const composer = (props) => {
   const getAssigneeName = _id => _id && Users.methods.fullNameWithTitle(Users.findOne({ _id }, { removed: true }) || {})
   const handleUpdate = (_id, update) => {
     Tags.update({ _id }, update)
-    Alert.success('ui.saved')
+    Alert.success(__('ui.saved'))
   }
   const handleInsert = (newTag) => {
     try {
       Tags.insert(newTag)
-      Alert.success('ui.saved')
+      Alert.success(__('ui.saved'))
     } catch (e) {
       console.error(e)
-      Alert.error('ui.error')
+      Alert.error(__('ui.error'))
     }
   }
   const handleRemove = _id => {
