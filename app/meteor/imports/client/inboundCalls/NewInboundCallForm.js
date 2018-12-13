1eb2f75d3ebe84c549ec84338f5ab1876a4283a8
Albert Zak <me@albertzak.com>
2018-12-13 15:10:23 +0100

    🦁 Make topic picker live

--- app/meteor/imports/client/inboundCalls/NewInboundCallForm.js
+++ app/meteor/imports/client/inboundCalls/NewInboundCallForm.js
@@ -29,12 +29,9 @@ class NewInboundCallFormComponent extends React.Component {
                   <Field name='telephone' component={TextField} fullWidth
                     label={__('inboundCalls.form.telephone.label')} />
                 </div>
-                {
-                  process.env.NODE_ENV === 'development' &&
-                    <div className='form-row'>
-                      <Field name='topicId' component={TopicPicker} />
-                    </div>
-                }
+                <div className='form-row'>
+                  <Field name='topicId' component={TopicPicker} />
+                </div>
               </div>
               <div className='col-md-6'>
                 <Field name='note'
