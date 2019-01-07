c2aec1861bcca5954b2d9168dedf33a224d1a695
Albert Zak <me@albertzak.com>
2019-01-07 23:06:05 +0100

    Add react-native-document-scanner package

--- app/camera/android/app/src/main/java/com/rosalindcamera/MainApplication.java
+++ app/camera/android/app/src/main/java/com/rosalindcamera/MainApplication.java
@@ -3,6 +3,7 @@ package com.rosalindcamera;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNPdfScannerPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import net.rhogan.rnsecurerandom.RNSecureRandomPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPdfScannerPackage(),
             new OrientationPackage(),
             new RNGestureHandlerPackage(),
             new RNSecureRandomPackage(),
