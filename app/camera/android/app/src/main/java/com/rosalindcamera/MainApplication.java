c26d92dbf2d2aaeac9f51d3180f2b552a00399ff
Albert Zak <me@albertzak.com>
2019-01-07 19:55:46 +0100

    Add gesture handler package and add screen orientation handler to build target

--- app/camera/android/app/src/main/java/com/rosalindcamera/MainApplication.java
+++ app/camera/android/app/src/main/java/com/rosalindcamera/MainApplication.java
@@ -3,6 +3,7 @@ package com.rosalindcamera;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import net.rhogan.rnsecurerandom.RNSecureRandomPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNSecureRandomPackage(),
             new RNCameraPackage()
       );
