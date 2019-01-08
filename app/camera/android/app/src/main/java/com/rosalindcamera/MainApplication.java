697181560af0f2910f7cea27a05d379cac949ed8
Albert Zak <me@albertzak.com>
2019-01-08 22:47:16 +0100

    Add react-native-perspective-image-cropper package

--- app/camera/android/app/src/main/java/com/rosalindcamera/MainApplication.java
+++ app/camera/android/app/src/main/java/com/rosalindcamera/MainApplication.java
@@ -3,6 +3,8 @@ package com.rosalindcamera;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNCustomCropPackage;
+import com.horcrux.svg.SvgPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import net.rhogan.rnsecurerandom.RNSecureRandomPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCustomCropPackage(),
+            new SvgPackage(),
             new OrientationPackage(),
             new RNGestureHandlerPackage(),
             new RNSecureRandomPackage(),
