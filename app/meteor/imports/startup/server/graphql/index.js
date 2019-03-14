027f3b6ac20d1d0cfd73e2c254fffe846586c94d
Albert Zak <me@albertzak.com>
2019-03-14 21:08:00 +0100

    🍕Resolve appointments and patients

--- app/meteor/imports/startup/server/graphql/index.js
+++ app/meteor/imports/startup/server/graphql/index.js
@@ -1,18 +1,24 @@
 import { ApolloServer } from 'apollo-server-express'
 import { WebApp } from 'meteor/webapp'
 import { getUser } from 'meteor/apollo'
-
 import typeDefs from './schema'
-import resolvers from './resolvers'
+import * as resolvers from './resolvers'
+import * as api from '../../../api'
 
 export default () => {
   const server = new ApolloServer({
     graphiql: true,
     typeDefs,
     resolvers,
-    context: async ({ req }) => ({
-      user: await getUser(req.headers.authorization)
-    })
+    context: async ({ req }) => {
+      const user = await getUser(req.headers.authorization)
+      if (!user || user.removed) {
+        console.error('[GraphQL] Access denied', req.ip)
+        throw new Error('Access denied')
+      }
+
+      return { user, api }
+    }
   })
 
   server.applyMiddleware({
--- app/meteor/imports/startup/server/graphql/schema.js
+++ app/meteor/imports/startup/server/graphql/schema.js
@@ -1,7 +1,34 @@
 import gql from 'graphql-tag'
 
 export default gql`
+  scalar Date
+
+  input DayInput {
+    year: Int!
+    month: Int!
+    day: Int!
+  }
+
+  type Patient {
+    _id: ID!
+    gender: String
+    titlePrepend: String
+    firstName: String
+    lastName: String!
+    appointments: [Appointment]
+  }
+
+  type Appointment {
+    _id: ID!
+    start: Date!
+    end: Date!
+    assigneeId: String
+    calendarId: String
+    patientId: String
+    patient: Patient
+  }
+
   type Query {
-    hi: String
+    getAppointments(calendarId: String!, day: DayInput!): [Appointment]
   }
 `
