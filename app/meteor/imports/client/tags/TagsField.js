62dbcc1d152d7549f218df4fedb27d8049174e1c
Albert Zak <me@albertzak.com>
2019-03-19 21:33:10 +0100

    ☄ Fix carrying tags from one assignee to another one where shouldn't be scheduled

--- app/meteor/imports/client/tags/TagsField.js
+++ app/meteor/imports/client/tags/TagsField.js
@@ -9,6 +9,79 @@ import { extendMoment } from 'moment-range'
 
 const moment = extendMoment(_moment)
 
+const getConstrainedTags = ({
+  allowedTags,
+  value,
+  maxDuration,
+  calendarId,
+  assigneeId,
+  time,
+  constraint
+}) => {
+  const selector = allowedTags ? { _id: { $in: allowedTags } } : {}
+  const tags = Tags.find(selector, { sort: { order: 1 } }).map((t) => {
+    const selected = value && value.includes(t._id)
+    return {
+      ...t,
+      selectable: true,
+      selected
+    }
+  }).filter(t => {
+    if (maxDuration === null) {
+      return true
+    }
+
+    const duration = getDefaultDuration({
+      calendarId,
+      assigneeId,
+      tags: [...(value || []), t._id],
+      date: moment(time)
+    })
+
+    if (duration > maxDuration) {
+      return false
+    } else {
+      return true
+    }
+  }).filter(t => {
+    if (t.maxParallel &&
+      Appointments.methods.getParallelAppointments({
+        start: time,
+        end: moment(time).clone().add(t.duration, 'minutes').subtract(1, 'second'),
+        tags: t._id
+      }).length >= t.maxParallel
+    ) {
+      return false
+    } else {
+      return true
+    }
+  }).filter(t => {
+    if ((!allowedTags || allowedTags.length === 0) && t.assigneeIds) {
+      return t.assigneeIds.includes(assigneeId)
+    } else {
+      return true
+    }
+  }).filter(t => {
+    if ((!allowedTags || allowedTags.length === 0) && t.blacklistAssigneeIds) {
+      return !t.blacklistAssigneeIds.includes(assigneeId)
+    } else {
+      return true
+    }
+  }).filter(t => {
+    if ((!allowedTags || allowedTags.length === 0) && t.calendarIds && calendarId) {
+      return t.calendarIds.includes(calendarId)
+    } else {
+      return true
+    }
+  })
+
+  const constrainedTags = constraint
+    ? applyConstraintToTags({ constraint, tags })
+    : tags
+
+  return constrainedTags
+}
+
 export class TagsField extends React.Component {
   constructor (props) {
     super(props)
@@ -30,9 +103,42 @@ export class TagsField extends React.Component {
     }
   }
 
+  // Check if any forbidden tags were passed as value prop and dispatch update accordingly
+  componentDidMount () {
+    const value = this.props.input.value
+    if (!value || value.length === 0) { return }
+
+    const {
+      allowedTags,
+      assigneeId,
+      calendarId,
+      maxDuration,
+      time,
+      constraint
+    } = this.props
+
+    const constrainedTags = getConstrainedTags({
+      allowedTags,
+      value,
+      assigneeId,
+      calendarId,
+      maxDuration,
+      time,
+      constraint
+    })
+
+    // BUG: Constrained tags with different durations may have same _id, this will always select the first matching tag only
+    const constrainedTagIds = constrainedTags.map(t => t._id)
+
+    const constrainedValue = value.filter(v => constrainedTagIds.includes(v))
+
+    console.log('[TagsField] Filtering tag selection from', value, 'to', constrainedValue)
+    this.props.input.onChange(constrainedValue)
+  }
+
   render () {
     const {
-      input,
+      input: { value },
       meta,
       assigneeId,
       allowedTags,
@@ -43,66 +149,15 @@ export class TagsField extends React.Component {
       constraint
     } = this.props
 
-    const selector = allowedTags ? { _id: { $in: allowedTags } } : {}
-    const tags = Tags.find(selector, { sort: { order: 1 } }).map((t) => {
-      const selected = input.value && input.value.includes(t._id)
-      return {
-        ...t,
-        selectable: true,
-        selected
-      }
-    }).filter(t => {
-      if (maxDuration === null) {
-        return true
-      }
-
-      const duration = getDefaultDuration({
-        calendarId,
-        assigneeId,
-        tags: [...(input.value || []), t._id],
-        date: moment(time)
-      })
-
-      if (duration > maxDuration) {
-        return false
-      } else {
-        return true
-      }
-    }).filter(t => {
-      if (t.maxParallel &&
-        Appointments.methods.getParallelAppointments({
-          start: time,
-          end: moment(time).clone().add(t.duration, 'minutes').subtract(1, 'second'),
-          tags: t._id
-        }).length >= t.maxParallel
-      ) {
-        return false
-      } else {
-        return true
-      }
-    }).filter(t => {
-      if ((!allowedTags || allowedTags.length === 0) && t.assigneeIds) {
-        return t.assigneeIds.includes(assigneeId)
-      } else {
-        return true
-      }
-    }).filter(t => {
-      if ((!allowedTags || allowedTags.length === 0) && t.blacklistAssigneeIds) {
-        return !t.blacklistAssigneeIds.includes(assigneeId)
-      } else {
-        return true
-      }
-    }).filter(t => {
-      if ((!allowedTags || allowedTags.length === 0) && t.calendarIds && calendarId) {
-        return t.calendarIds.includes(calendarId)
-      } else {
-        return true
-      }
+    const constrainedTags = getConstrainedTags({
+      allowedTags,
+      value,
+      assigneeId,
+      calendarId,
+      maxDuration,
+      time,
+      constraint
     })
-
-    const constrainedTags = constraint
-      ? applyConstraintToTags({ constraint, tags })
-      : tags
 
     return <div>
       <TagsList
