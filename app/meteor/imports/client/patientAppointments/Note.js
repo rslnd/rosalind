1b647e975ee3e08772485d0631b900350a2c8e24
Albert Zak <me@albertzak.com>
2019-05-01 22:16:23 +0200

    🐨 Add fallback for comments to note migration

--- app/meteor/imports/client/patientAppointments/Note.js
+++ app/meteor/imports/client/patientAppointments/Note.js
@@ -1,15 +1,33 @@
+import identity from 'lodash/identity'
 import React from 'react'
-import { withHandlers } from 'recompose'
+import { withHandlers, compose } from 'recompose'
 import { Comments } from '../../api/comments'
 import { updateAppointment } from './updateAppointment'
 import { Textarea } from './Field'
+import { withTracker } from '../components/withTracker'
 
-// TODO: Find a way to migrate, or clean up?
-export const Note = withHandlers({
-  updateNote: props => note => updateAppointment(props, { note })
-  // Comments.find({ docId: _id }).fetch().map(a => <div key={a._id}>{a.body}</div>
-})(({ _id, note, updateNote }) =>
+// This is a last resort fallback if the migration did not work as expected
+// TODO: Remove after a few weeks in prod whithout 'Missing comments' errors
+export const Note = compose(
+  withTracker(props => {
+    const comments = Comments.find({ docId: props._id }).fetch()
+
+    const currentNote = props.note || ''
+    const missingComments = comments.filter(c => currentNote.indexOf(c.body) === -1)
+
+    if (missingComments.length > 0) {
+      console.error(`Missing comments for apppointmentId ${props._id}`)
+    }
+
+    const note = [currentNote, missingComments].filter(identity).join('\n\n').trim()
+    return { note }
+  }),
+  withHandlers({
+    updateNote: props => note => updateAppointment(props, { note })
+  })
+)(({ _id, note, updateNote, isCurrent }) =>
   <Textarea
+    blankRows={isCurrent ? 1 : 0}
     initialValue={note}
     onChange={updateNote}
     style={noteStyle}
