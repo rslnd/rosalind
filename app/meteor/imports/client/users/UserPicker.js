6066974224c5be42d23430e189fc3abb6b1325b7
Albert Zak <me@albertzak.com>
2019-05-02 09:50:35 +0200

    🍠 Allow user picker to select non-employees, and hide removed users

--- app/meteor/imports/client/users/UserPicker.js
+++ app/meteor/imports/client/users/UserPicker.js
@@ -6,7 +6,7 @@ export const UserPicker = withProps({
   toDocument: _id => Users.findOne({ _id }, { removed: true }),
   toLabel: user => Users.methods.fullNameWithTitle(user),
   options: () => {
-    const selector = { groupId: { $ne: null }, employee: true }
+    const selector = { groupId: { $ne: null }, removed: { $ne: true } }
     return Users.find(selector, {
       sort: { lastName: 1 }
     }).fetch()
