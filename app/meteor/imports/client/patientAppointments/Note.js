bc4a1616b6a649904a4c70633898c13d854b0c1f
Albert Zak <me@albertzak.com>
2019-05-14 12:50:05 +0200

    👑 Remove missing comments check

--- app/meteor/imports/client/patientAppointments/Note.js
+++ app/meteor/imports/client/patientAppointments/Note.js
@@ -1,33 +1,13 @@
-import identity from 'lodash/identity'
 import React from 'react'
 import { withHandlers, compose } from 'recompose'
-import { Comments } from '../../api/comments'
 import { updateAppointment } from './updateAppointment'
 import { Textarea } from './Field'
-import { withTracker } from '../components/withTracker'
 
-// This is a last resort fallback if the migration did not work as expected
-// TODO: Remove after a few weeks in prod whithout 'Missing comments' errors
+// It's not a bug when we find a comment that looks like it was not migrated
+// to the appointment note field, but rather the appointment note was changed
+// in the meantime. (used to trigger `missing comment for appointmentId` errors)
+// and re-add the comment.
 export const Note = compose(
-  withTracker(props => {
-    const comments = Comments.find({ docId: props._id }).fetch()
-
-    const currentNote = props.note || ''
-    const missingComments = comments.map(c => c.body).filter(b => currentNote.indexOf(b) === -1)
-
-    if (missingComments.length > 0) {
-      console.error(`Missing comments for apppointmentId ${props._id}`, missingComments)
-    }
-
-    const note = [
-      currentNote,
-      ...missingComments
-    ].filter(identity)
-      .join('\n\n')
-      .replace(/\[object Object\]/g, '') // Hacky
-
-    return { ...props, note }
-  }),
   withHandlers({
     updateNote: props => note => updateAppointment(props, { note })
   })
