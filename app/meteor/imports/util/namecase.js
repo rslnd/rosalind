8d3f6c26a3ffd03a614851f55352728723570e7b
Albert Zak <me@albertzak.com>
2019-06-26 12:25:08 +0200

    ⛄️ Add optional pseudonymization/demo mode in front end

--- app/meteor/imports/util/namecase.js
+++ app/meteor/imports/util/namecase.js
@@ -1,12 +1,30 @@
 import nc from 'namecase'
+import { pseudonyms } from './pseudonyms'
 
 const checkName = a =>
   a === a.toUpperCase() || a === a.toLowerCase()
 
 export const namecase = (a = '') => {
+  if (window.pseudonymize) {
+    return pseudonym(a)
+  }
+
   if (checkName(a)) {
     return nc(a)
   } else {
     return a
   }
 }
+
+const pseudonymCache = {}
+const pseudonym = a => {
+  if (pseudonymCache[a]) {
+    return pseudonymCache[a]
+  }
+
+  pseudonymCache[a] = randomName()
+  return pseudonymCache[a]
+}
+
+const randomName = () =>
+  pseudonyms[Math.floor(Math.random() * (pseudonyms.length - 1))]
