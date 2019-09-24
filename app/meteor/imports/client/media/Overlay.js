2494630abf65edf8c4caf2249f0ccbd6bc9d8895
Albert Zak <me@albertzak.com>
2019-09-24 18:43:16 +0200

    🧧 Make media dimensions available to transformation calculation

--- app/meteor/imports/client/media/Overlay.js
+++ app/meteor/imports/client/media/Overlay.js
@@ -30,6 +30,8 @@ export const MediaOverlay = compose(
     if (ref.current) {
       ref.current.style.transform = imageTransform({
         zoomed: (overrideZoomed === null ? zoomed : overrideZoomed),
+        mediaHeight: currentMedia.height,
+        mediaWidth: currentMedia.width,
         mouseX: e.nativeEvent.clientX,
         mouseY: e.nativeEvent.clientY,
         rotation: currentMedia.rotation
@@ -67,7 +69,12 @@ export const MediaOverlay = compose(
               ref={ref}
               onClick={toggleZoom}
               src={currentMedia.url}
-              style={imgStyle({ zoomed, rotation: currentMedia.rotation })} />
+              style={imgStyle({
+                zoomed,
+                rotation: currentMedia.rotation,
+                mediaWidth: currentMedia.width,
+                mediaHeight: currentMedia.height
+              })} />
           </div>
         }
       </div>
@@ -117,20 +124,27 @@ const fitToScreen = {
   padding: 30
 }
 
-const imgStyle = ({ zoomed, rotation }) => ({
+const imgStyle = ({ zoomed, rotation, mediaWidth, mediaHeight }) => ({
   maxWidth: '100%',
   maxHeight: '100%',
   cursor: zoomed ? 'zoom-out' : 'zoom-in',
   transition: 'transform 300ms cubic-bezier(0.1, 0.81, 0.24, 1)',
-  transform: imageTransform({ zoomed, rotation })
+  transform: imageTransform({ zoomed, rotation, mediaWidth, mediaHeight })
 })
 
-const imageTransform = ({ zoomed, mouseX, mouseY, rotation = 0 }) => {
+const imageTransform = ({ zoomed, mouseX, mouseY, mediaWidth, mediaHeight, rotation = 0 }) => {
   if (!zoomed) { return `rotate(${rotation}deg)` }
 
   const tX = (((window.innerWidth / 2) - mouseX) * 2) + 'px'
   const tY = (((window.innerHeight / 2) - mouseY) * 2) + 'px'
-  const transform = `translateX(${tX}) translateY(${tY}) translateZ(0) scale(2) rotate(${rotation}deg)`
+
+  const transform = `
+    translateX(${tX})
+    translateY(${tY})
+    translateZ(0)
+    scale(2)
+    rotate(${rotation}deg)`
+
   return transform
 }
 
