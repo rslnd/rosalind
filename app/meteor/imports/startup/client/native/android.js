226c4d6cc2f63591de90379e849bc9d74721a406
Albert Zak <me@albertzak.com>
2019-10-09 12:39:47 +0200

    🧜🏻‍♂️ Add legacy android bridge

--- app/meteor/imports/startup/client/native/android.js
+++ app/meteor/imports/startup/client/native/android.js
@@ -12,9 +12,41 @@ const toNative = [
 ]
 
 export const handleAndroidEvents = ({ events }) => {
+  if (isLegacyAndroid()) {
+    console.log('[Native] legacyAndroid handling events')
+
+    const attemptInstallHandler = () => {
+      if (window.native && window.native.events && window.native.events.on) {
+        console.log('[Native] legacyAndroid Registered event handler')
+
+        setTimeout(() => {
+          console.log('[Native] legacyAndroid Posting welcome')
+          events.emit('welcome', {
+            clientKey: window.native.clientKey,
+            systemInfo: {
+              bridge: 'androidLegacy',
+              ...window.native.systemInfo
+            }
+          })
+        }, 100)
+      } else {
+        window.requestAnimationFrame(attemptInstallHandler)
+      }
+    }
+    window.requestAnimationFrame(attemptInstallHandler)
+
+    window.native.load()
+
+    return {
+      postToNative: postToNativeLegacy(events)
+    }
+  }
+
   if (window.rslndAndroid) {
     // Native->Web: Expose global function to post events via injected code
     window.rslndAndroidPostToWeb = postToWeb(events)
+
+    console.log('[Native] Handling Android events')
 
     return {
       postToNative: postToNative(events)
@@ -48,3 +80,17 @@ const postToWeb = events => (name, payload) => {
   console.log(`[Native] Android: Emitting event ${name}`)
   events.emit(name, payload)
 }
+
+// Legacy handlers
+
+const postToNativeLegacy = events => (name, payload) => {
+  console.log(`[Native] legacyAndroid: Posting to native ${name}`)
+  if (window.native.events) {
+    window.native.events.emit(name, payload)
+  } else {
+    console.log('[Native] legacyAndroid: EventEmitter not loaded yet, discarding event')
+  }
+}
+
+const isLegacyAndroid = () =>
+  window.native && window.native.android && window.native.load
