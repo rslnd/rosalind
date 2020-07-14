2ffbde16dc0e27a96fb07219d68c94b44b4e05de
Albert Zak <me@albertzak.com>
2020-07-14 19:15:10 +0200

    🧬 Log to logdna, bump

--- app/electron/main/logger.js
+++ app/electron/main/logger.js
@@ -1,86 +1,82 @@
 const os = require('os')
+const fs = require('fs')
 const path = require('path')
-const url = require('url')
 const includes = require('lodash/includes')
-const winston = require('winston')
-require('winston-papertrail')
+const LogDNA = require('logdna')
 const { ipcMain, app } = require('electron')
 const { inspect } = require('util')
 
-const PAPERTRAIL_URL = 'logs4.papertrailapp.com:20784'
+const K = '840559bea375dff05893d84878a50d93'
 
-const start = () => {
-  winston.add(winston.transports.File, {
-    filename: path.join(app.getPath('userData'), 'RosalindElectron.log'),
-    handleExceptions: true,
-    humanReadableUnhandledException: true,
-    level: 'debug',
-    json: false,
-    maxsize: 1024 * 1024 * 10,
-    maxFiles: 1,
-    eol: '\r\n'
-  })
+const program = [ [ 'rosalind', os.platform(), os.arch() ].join('-'), app.getVersion() ].join('/')
 
-  const host = PAPERTRAIL_URL.split(':')[0]
-  const port = parseInt(PAPERTRAIL_URL.split(':')[1])
+const logger = LogDNA.createLogger(K, {
+  hostname: os.hostname(),
+  app: program,
+  env: process.env.NODE_ENV,
+  tags: ['electron']
+})
 
-  if (host && port) {
-    let customerHostname = 'development'
-    try {
-      const settings = require('./settings')
-      customerHostname = url.parse(settings.url).hostname
-    } catch (e) {
-      winston.error('[Log] Could not parse customer hostname for centralized logging, falling back to "development"', e)
+
+const ensureOldLogsRemoved = () => {
+  const oldPath = path.join(app.getPath('userData'), 'RosalindElectron.log')
+
+  fs.exists(oldPath, (exists) => {
+    if (exists) {
+      try {
+        fs.unlink(oldPath)
+      } catch (e) {
+
+      }
     }
-
-    const hostname = [os.hostname(), customerHostname].join('.')
-    const program = [ [ 'rosalind', os.platform(), os.arch() ].join('-'), app.getVersion() ].join('/')
-    winston.info('[Log] Enabling papertrail log transport', { program, hostname })
-    winston.add(winston.transports.Papertrail, { host, port, program, hostname })
-  }
-
-  winston.info('[Log] App launched in: ', process.execPath)
-  winston.info('[Log] App version: ', app.getVersion())
-  winston.info('[Log] Command line arguments: ', process.argv)
-
-  process.on('uncaughtException', err =>
-    winston.error('[Main] Uncaught exception:', err)
-  )
-
-  process.on('unhandledRejection', err =>
-    winston.error('[Main] Unhandled rejection:', err)
-  )
-
-  app.on('quit', () =>
-    winston.info('[Log] App quit')
-  )
-
-  ipcMain.on('log', (e, msg) => {
-    winston.info(`[ipc] ${inspect(msg)}`)
   })
 }
 
+const start = () => {
+  logger.info(`[Log] App launched in: ${process.execPath}`)
+  logger.info(`[Log] App version: ${app.getVersion()}`)
+  logger.info(`[Log] Command line arguments: ${process.argv.join(' ')}`)
+
+  process.on('uncaughtException', err =>
+    logger.error('[Main] Uncaught exception:', {meta: err})
+  )
+
+  process.on('unhandledRejection', err =>
+    logger.error('[Main] Unhandled rejection:', {meta: err})
+  )
+
+  app.on('quit', () =>
+    logger.info('[Log] App quit')
+  )
+
+  ipcMain.on('log', (e, msg) => {
+    logger.info(`[ipc] ${inspect(msg)}`)
+  })
+
+  ensureOldLogsRemoved()
+}
+
 const ready = log => {
-  winston.info(log)
+  logger.info(log)
 
   if (includes(process.argv, '--debug-quit-on-ready')) {
-    winston.info('[Log] Debug: App launched successfully; now quitting')
+    logger.info('[Log] Debug: App launched successfully; now quitting')
     app.quit()
   }
 }
 
-const formatLog = logger => (...logs) => {
+const formatLog = logFn => (...logs) => {
   // JSON.stringify may throw on circular object references
   const message = logs.map(l => inspect(l)).join(' ')
-  logger(message)
+  logFn(message)
   return message
 }
 
 module.exports = {
   start,
   ready,
-  debug: formatLog(winston.debug),
-  info: formatLog(winston.info),
-  warn: formatLog(winston.warn),
-  error: formatLog(winston.error)
+  debug: formatLog(logger.debug),
+  info: formatLog(logger.info),
+  warn: formatLog(logger.warn),
+  error: formatLog(logger.error)
 }
