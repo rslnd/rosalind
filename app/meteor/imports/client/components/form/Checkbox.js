dafb474747e2c92da1a430a726bd2b1dc596aafc
Albert Zak <me@albertzak.com>
2020-07-19 21:41:47 +0200

    💢 Fix double label wrapping checkbox

--- app/meteor/imports/client/components/form/Checkbox.js
+++ app/meteor/imports/client/components/form/Checkbox.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import MuiCheckbox from '@material-ui/core/Checkbox'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
 
-export const Checkbox = ({ value, onChange, label, input }) =>
-  <div>
-    <FormControlLabel
-      control={
-        <MuiCheckbox
-          checked={(value || (input && input.value)) ? true : false}
-          onChange={(onChange || (input && input.onChange))}
-        />
-      }
-      label={label}
-    />
-  </div>
+export const Checkbox = ({ value, onChange, input }) =>
+  <MuiCheckbox
+    checked={(value || (input && input.value)) ? true : false}
+    onChange={(onChange || (input && input.onChange))}
+  />
