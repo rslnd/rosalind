42f7b66a4f637b3db6d4c5dd3185459e74d37ceb
Albert Zak <me@albertzak.com>
2020-12-13 16:30:22 +0100

    👩‍👩‍👦 Add patients/isExternalHashDifferent action

--- app/meteor/imports/api/patients/actions/index.js
+++ app/meteor/imports/api/patients/actions/index.js
@@ -6,6 +6,7 @@ import { setBirthday } from './setBirthday'
 import { setContacts } from './setContacts'
 import { setAgreement } from './setAgreement'
 import { upsert } from './upsert'
+import { isExternalHashDifferent } from './isExternalHashDifferent'
 
 export default function ({ Patients }) {
   return {
@@ -16,6 +17,7 @@ export default function ({ Patients }) {
     setBirthday: setBirthday({ Patients }),
     setContacts: setContacts({ Patients }),
     setAgreement: setAgreement({ Patients }),
-    upsert: upsert({ Patients })
+    upsert: upsert({ Patients }),
+    isExternalHashDifferent: isExternalHashDifferent({ Patients })
   }
 }
---
+++ app/meteor/imports/api/patients/actions/isExternalHashDifferent.js
@@ -0,0 +1,37 @@
+import { action } from '../../../util/meteor/action'
+import { Events } from '../../events'
+
+// Filters a given list of hashes and returns only those hashes that do not match any patient
+export const isExternalHashDifferent = ({ Patients }) =>
+  action({
+    name: 'patients/isExternalHashDifferent',
+    allowAnonymous: true,
+    requireClientKey: true,
+    args: {
+      externalHashes: [String],
+      externalProvider: 'inno'
+    },
+    fn ({ externalHashes, externalProvider }) {
+      Events.post('patients/isExternalHashDifferent')
+      console.log(`[Patients] isExternalHashDifferent: processing ${externalHashes.length} external hashes`)
+      const startAt = new Date()
+
+      const field = `external.${externalProvider}.hash`
+      const upToDatePatients = Patients.find({
+        [field]:{ $in: externalHashes }
+      }, { fields: {
+        [field]: 1
+      }}).fetch().map(p => p.external[externalProvider].hash)
+
+      let upToDatePatientsHashmap = {}
+      upToDatePatients.forEach(h => {
+        upToDatePatientsHashmap[h] = true
+      })
+
+      const differentExternalHashes = externalHashes.filter(h => !upToDatePatientsHashmap[h])
+
+      console.log(`[Patients] isExternalHashDifferent: processing ${externalHashes.length} external hashes to get ${differentExternalHashes.length} different hashes took ${(new Date() - startAt) / 1000} seconds`)
+
+      return differentExternalHashes
+    }
+  })
