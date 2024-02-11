bc16a09dd95929d87da32a4a1000ec4da44e68d7
Albert Zak <me@albertzak.com>
2024-02-11 22:16:40 +0100

    Fix appt count with bookables

--- app/meteor/imports/client/appointments/dayView/grid/AppointmentsGrid.js
+++ app/meteor/imports/client/appointments/dayView/grid/AppointmentsGrid.js
@@ -64,7 +64,7 @@ export const AppointmentsGrid = ({
   return (
     <div style={style}>
       <ErrorBoundary silent name='AppG Counts'>
-        {renderCounts({ calendar, assignees, appointments, schedules, gridTimeSlots })}
+        {renderCounts({ date, calendar, assignees, appointments, schedules, gridTimeSlots })}
       </ErrorBoundary>
 
       {renderAppointments({ calendar, slotSize, scheduleOffset, atMinutes, appointments, onClick: onAppointmentClick, move, canSeeBookables, canEditBookables })}
@@ -77,7 +77,7 @@ export const AppointmentsGrid = ({
   )
 }
 
-const renderCounts = ({ assignees, calendar, appointments, schedules, gridTimeSlots }) => {
+const renderCounts = ({ date, assignees, calendar, appointments, schedules, gridTimeSlots }) => {
   const { showCounts, slotSize, slotSizeAppointment, allowUnassigned, atMinutes } = calendar
   if (!showCounts) { return null }
 
@@ -106,28 +106,39 @@ const renderCounts = ({ assignees, calendar, appointments, schedules, gridTimeSl
 
     // const slots = timeSlots(slotSizeBlank, scheduleOffset, atMinutes)
 
-    const assigneeAppointments = labeledAppointments.filter(ap => (ap.assigneeId === assigneeId))
+    const assigneeAppointments = labeledAppointments.filter(ap =>
+      (ap.assigneeId === assigneeId) &&
+      (ap.type !== 'bookable')
+    )
 
     const admittedCount = assigneeAppointments.filter(ap => ap.admittedAt).length
     const noShowCount = assigneeAppointments.length - admittedCount
 
 
-    const assigneeSchedules = schedules.filter(s => (
+    const assigneeSchedules = sortBy(schedules.filter(s => (
       (s.userId === assigneeId) &&
+      (s.calendarId === calendar._id) &&
       !s.available &&
       (!s.roles || s.roles.length === 0)
-    ))
+    )), 'start')
 
     const blockedMs = assigneeSchedules.reduce((acc, s) => (acc + (s.end - s.start)), 0)
     const apptMs = assigneeAppointments.reduce((acc, a) => (acc + (a.end - a.start)), 0)
 
     const wholeDayMs = assigneeSchedules.length >= 2
       ? (assigneeSchedules[assigneeSchedules.length - 1].end - assigneeSchedules[0].start)
-      : ((22 - 7.5) * 60 * 60 * 1000)
+      : ((22 - 7.5 - 1) * 60 * 60 * 1000)
 
     const wholeDayHrs = durationFormat(wholeDayMs - blockedMs, 'ms')
     const freeHrs =  durationFormat(wholeDayMs - blockedMs - apptMs, 'ms')
 
+
+    console.log({ assigneeSchedules: assigneeSchedules.length,
+       assigneeAppointments: assigneeAppointments.length,
+       blockedMs: durationFormat(blockedMs, 'ms'),
+       apptMs: durationFormat(apptMs, 'ms'),
+       wholeDayMs: durationFormat(wholeDayMs, 'ms'),
+       freeHrs })
 
     // let gridTimeSlots2 =
     // for (let i = 0; i < gridTimeSlots.length; i++) {
@@ -160,8 +171,16 @@ const renderCounts = ({ assignees, calendar, appointments, schedules, gridTimeSl
     return <>
       <div style={style} className='enable-select text-muted'>
         <span>
-          {freeHrs} frei von {wholeDayHrs} <br/>
-          {admittedCount} <Icon name="check" /> &emsp; {noShowCount} <Icon name="times" />
+          {(assigneeId &&
+            <>{freeHrs} frei von {wholeDayHrs} <br/></>)}
+
+
+          {
+            moment().isSameOrAfter(date, 'day')
+            ? <>{admittedCount} <Icon name="check" /> &emsp; {noShowCount} <Icon name="times" /></>
+            : <>{admittedCount + noShowCount} <Icon name="user" /></>
+          }
+
 
         </span>
         <br />
