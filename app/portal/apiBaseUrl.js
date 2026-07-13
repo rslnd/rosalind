4c89d92532c63e7e137612ce19c5eb79712eb10a
Christina Felicitas Zak <cfz@fixpoint.co.at>
2026-07-13 19:06:15 +0200

    Update portal

--- app/portal/apiBaseUrl.js
+++ app/portal/apiBaseUrl.js
@@ -1,5 +1,5 @@
 const isProduction = (typeof window !== 'undefined' ? (window.location.protocol === 'https:') : true)
 
-export const apiBaseUrl = isProduction ? '' : 'http://10.0.0.69:3000'
+export const apiBaseUrl = isProduction ? '' : 'http://10.0.0.73:3000'
 
 console.log(apiBaseUrl)
